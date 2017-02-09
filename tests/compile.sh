1102e3a2d6232945b3db425a4993703989ca489d
samuel.trahan@noaa.gov <samuel.trahan@noaa.gov>
2017-02-09 19:43:27 +0000

    Move conf and modules to application level.  Use NEMS/src/configure to write NEMS/src/conf areas.  UNTESTED due to Surge downtime.

--- tests/compile.sh
+++ tests/compile.sh
@@ -11,49 +11,42 @@ fi
 readonly PATHTR=$1
 readonly MACHINE_ID=$2
 readonly MAKE_OPT=$3
-readonly BUILD_NR=$4
+readonly BUILD_NAME=fv3${4:+_$4}
+
+readonly PATHNEMS="$PATHTR/../NEMS"
 
 set +x
 hostname
 
 echo "Compiling ${MAKE_OPT}"
+
+# Configure FV3
 cd ${PATHTR}
-
 ./configure ${MACHINE_ID}
-cp conf/configure.fv3 $PATHTR/../NEMS/src/conf
-cat $PATHTR/../NEMS/src/conf/configure.nems.NUOPC conf/configure.fv3 >$PATHTR/../NEMS/src/conf/configure.nems
-cat << EOF >> $PATHTR/../NEMS/src/conf/configure.nems
-  FFLAGS += \$(ESMF_INC)
-  CPPFLAGS += -traditional
-  EXTLIBS = \$(NCEPLIBS) \$(ESMF_LIB) \$(LDFLAGS)
-EOF
 
-set +x
-if [[ $MACHINE_ID = wcoss ]]; then
-  source /usrx/local/Modules/default/init/sh
-elif [[ $MACHINE_ID = wcoss_cray ]]; then
-  source /opt/modules/default/init/sh
-elif [[ $MACHINE_ID = theia ]]; then
-  source /apps/lmod/lmod/init/sh
-fi
-source conf/modules.fv3
+# Configure NEMS
+cd $PATHNEMS
+src/configure conf/configure.fv3.$MACHINE_ID \
+              modulefiles/$MACHINE_ID/fv3
+
+# Load the "module" command, purge modules, and load modules
+source $PATHNEMS/src/conf/modules.nems.sh
 module list
-set -x
 
+# Build FV3
+cd $PATHTR
 gmake clean
-
 gmake ${MAKE_OPT} -j 8 nemsinstall
 
-cd $PATHTR/../NEMS/src
-touch conf/externals.nems
-cp ESMFVersionDefine_ESMF_NUOPC.h ESMFVersionDefine.h
+# Build NEMS
+cd $PATHNEMS/src
 export COMP=FV3                     
 export COMP_SRCDIR=$PATHTR           
 export COMP_BINDIR=$PATHTR/FV3_INSTALL
 gmake nems COMP=,fv3, FV3_DIR=$COMP_BINDIR
 
-cp ../exe/NEMS.x ${PATHTR}/../tests/fv3_${BUILD_NR}.exe
-cp ${PATHTR}/conf/modules.fv3 ${PATHTR}/../tests/modules.fv3_${BUILD_NR}
+cp ../exe/NEMS.x ${PATHTR}/../tests/$BUILD_NAME.exe
+cp $PATHNEMS/conf/modules.nems ${PATHTR}/../tests/modules.$BUILD_NAME
 
 gmake clean
 cd $PATHTR
