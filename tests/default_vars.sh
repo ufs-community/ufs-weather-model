2fbb6d10845306ad6120e9523b4746fd9619143e
jun.wang@noaa.gov <jun.wang@noaa.gov>
2017-08-14 14:15:32 +0000

    TRUNK UPDATE, fv3 ticket 4: fv3 write grid component
     M      .
    X       NEMS
    X       FV3
    M       parm/stretched-nest-model_configure.IN
    M       parm/model_configure.IN
    M       parm/input.nml.IN
    M       parm/stretched-model_configure.IN
    M       log/report-theia-log/rtreport.txt
    M       log/report-theia-log/build_fv3_32bit.exe.log
    M       log/report-theia-log/build_fv3_appbuilder.exe.log
    M       log/report-theia-log/build_fv3.exe.log
    M       log/report-xjet-log/rtreport.txt
    M       log/report-xjet-log/build_fv3_32bit.exe.log
    M       log/report-xjet-log/build_fv3_appbuilder.exe.log
    M       log/report-xjet-log/build_fv3.exe.log
    M       log/report-wcoss.cray-log/rtreport.txt
    M       log/report-wcoss.cray-log/build_fv3_32bit.exe.log
    M       log/report-wcoss.cray-log/build_fv3_appbuilder.exe.log
    M       log/report-wcoss.cray-log/build_fv3.exe.log
    M       log/report-wcoss.phase1-log/rtreport.txt
    M       log/report-wcoss.phase1-log/build_fv3_32bit.exe.log
    M       log/report-wcoss.phase1-log/build_fv3_appbuilder.exe.log
    M       log/report-wcoss.phase1-log/build_fv3.exe.log
    M       log/report-wcoss.phase2-log/rtreport.txt
    M       log/report-wcoss.phase2-log/build_fv3_32bit.exe.log
    M       log/report-wcoss.phase2-log/build_fv3_appbuilder.exe.log
    M       log/report-wcoss.phase2-log/build_fv3.exe.log
     M      tests
    M       tests/RegressionTests_wcoss_cray.log
    M       tests/Compile_theia.log
    M       tests/default_vars.sh
    M       tests/RegressionTests_theia.log
    M       tests/rt.sh
    M       tests/tests/fv3_read_inc
    M       tests/tests/fv3_restart
    M       tests/tests/fv3_decomp
    M       tests/tests/fv3_2threads
    M       tests/tests/fv3_appbuild
    M       tests/tests/fv3_control
    M       tests/fv3_conf/fv3_qsub.IN_theia
    M       tests/fv3_conf/fv3_run.IN
    M       tests/Compile_wcoss_cray.log
    M       conf/configure.fv3.jet
    M       conf/configure.fv3.theia
    M       conf/configure.fv3.wcoss_cray
    M       conf/configure.fv3.wcoss_phase2
     M      doc
    M       modulefiles/wcoss_phase2/fv3
    M       modulefiles/jet/fv3
    M       modulefiles/theia/fv3
    M       modulefiles/wcoss_cray/fv3
    M       compsets/jet.input
    M       compsets/platforms.input
    M       compsets/fv3.input
    M       compsets/wcoss.input
    
    Performing status on external item at 'FV3'
    
    Performing status on external item at 'NEMS'
    X       NEMS/tests/produtil

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -15,14 +15,14 @@ if [ $MACHINE_ID = wcoss ]; then
 
 elif [ $MACHINE_ID = wcoss_cray ]; then
 
-  TASKS_dflt=144 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
-  TASKS_thrd=72  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_dflt=150 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
   TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
   TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
 elif [ $MACHINE_ID = theia ]; then
 
-  TASKS_dflt=144 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
-  TASKS_thrd=72  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_dflt=150 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
+  TASKS_thrd=84  ; TPN_thrd=12 ; INPES_thrd=3 ; JNPES_thrd=4
   TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
   TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
 
@@ -37,6 +37,12 @@ export INPES=$INPES_dflt
 export JNPES=$JNPES_dflt
 export TASKS=$TASKS_dflt
 export TPN=$TPN_dflt
+export QUILTING=.true.
+export WRITE_GROUP=1
+export WRTTASK_PER_GROUP=6
+export NUM_FILES=2
+export FILENAME_BASE="'dyn' 'phy'"
+
 export WARM_START=.F.
 export READ_INCREMENT=.F.
 export NGGPS_IC=.T.
@@ -45,8 +51,13 @@ export MAKE_NH=.T.
 export MOUNTAIN=.F.
 export NA_INIT=1
 export DAYS=1
-export FDIAG=0,1,2,3,4,5,6,7,8,9,10,11,12,18,24
+export NPX=97
+export NPY=97
 export NSTF_NAME=2,1,1,0,5
+export FDIAG=0,1,2,3,4,5,6,7,8,9,10,11,12,15,18,21,24
+export NFHOUT=3
+export NFHMAX_HF=12
+export NFHOUT_HF=1
 
 
 export ENS_NUM=1
@@ -56,5 +67,6 @@ export SDAY=03
 export SHOUR=00
 export FHMAX=`expr $DAYS \* 24`
 export DT_ATMOS=1800
+
 
 }
--- tests/rt.sh
+++ tests/rt.sh
@@ -182,7 +182,7 @@ while getopts ":cfsl:mreh" opt; do
   esac
 done
 
-RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/trunk-20170629}
+RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/trunk-20170714}
 
 shift $((OPTIND-1))
 [[ $# -gt 0 ]] && usage
