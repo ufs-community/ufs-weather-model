be59fbf20d3b27aa8c90839dc8c32c21e0cc97dc
jun.wang <Jun.Wang@noaa.gov>
2018-05-26 03:15:12 +0000

    NEMSfv3gfs: this commits #refs 50871 and 50895
    Change-Id: I557619b3b4ac9b30fb91ead2eba8d10e9de30dc0

--- tests/rt.sh
+++ tests/rt.sh
@@ -233,7 +233,7 @@ while getopts ":cfsl:mreh" opt; do
   esac
 done
 
-RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/trunk-20180507}
+RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/trunk-20180524}
 
 shift $((OPTIND-1))
 [[ $# -gt 1 ]] && usage
