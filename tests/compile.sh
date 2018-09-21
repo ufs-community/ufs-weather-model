12404e33bded8616017a3c7f95d020b931fa81d6
Samuel Trahan <Samuel.Trahan@noaa.gov>
2018-09-21 18:33:52 +0000

    Remove FMS from MOM6 and FV3.  Add it to the application level.

--- tests/compile.sh
+++ tests/compile.sh
@@ -9,23 +9,55 @@ if [[ $# -lt 2 ]]; then
   exit 1
 fi
 
+# ----------------------------------------------------------------------
+# Make sure we have a "make" and reasonable threads.
+
+make=gmake
+if ( ! which $make ) ; then
+    echo WARNING: Cannot find gmake in \$PATH.  I will use \"make\" instead.
+    make=make
+    if ( ! $make --version 2>&1 | grep -i gnu > /dev/null 2>&1 ) ; then
+        echo WARNING: The build system requires GNU Make. Things may break.
+    fi
+fi
+
+if [[ $BUILD_TARGET == cheyenne.* ]] ; then
+    MAKE_THREADS=${MAKE_THREADS:-3}
+fi
+
+MAKE_THREADS=${MAKE_THREADS:-8}
+
+if [[ "$MAKE_THREADS" -gt 1 ]] ; then
+    echo Using \$MAKE_THREADS=$MAKE_THREADS threads to build FV3 and FMS.
+    echo Consider reducing \$MAKE_THREADS if you hit memory or process limits.
+fi
+
+# ----------------------------------------------------------------------
+# Parse arguments.
+
 set -xeu
 
 readonly PATHTR=$1
 readonly BUILD_TARGET=$2
 readonly MAKE_OPT=${3:-}
-#readonly BUILD_NAME=${4:-}
 readonly BUILD_NAME=fv3${4:+_$4}
 
 clean_before=${5:-YES}
 clean_after=${6:-YES}
 
 readonly PATHNEMS="$PATHTR/../NEMS"
+readonly FMS_SRCDIR=$PATHNEMS/../FMS
+readonly FMS_BINDIR=$PATHNEMS/../FMS/FMS_INSTALL
 
 #set +x
 hostname
 
+# ----------------------------------------------------------------------
+
 echo "Compiling ${MAKE_OPT} into $BUILD_NAME.exe on $BUILD_TARGET"
+
+# ----------------------------------------------------------------------
+# Configure NEMS and components
 
 # Configure NEMS
 cd $PATHNEMS
@@ -40,9 +72,9 @@ fi
 
 # Load the "module" command, purge modules, and load modules
 source $PATHNEMS/src/conf/modules.nems.sh
-if [[ $BUILD_TARGET != "macosx.gnu" ]]; then
-  module list
-fi
+set +e
+module list
+set -e
 
 # Build CCPP
 if [[ "${MAKE_OPT}" == *"CCPP=Y"* ]]; then
@@ -65,51 +97,51 @@ fi
 cp -fp $PATHNEMS/src/conf/configure.nems $PATHTR/conf/configure.fv3
 cp -fp $PATHNEMS/src/conf/modules.nems $PATHTR/conf/modules.fv3
 
-# Build FV3
-cd $PATHTR
-if [[ $BUILD_TARGET == "macosx.gnu" ]]; then
-  if [ $clean_before = YES ] ; then make clean ; fi
-  make ${MAKE_OPT} -j 8 nemsinstall
-else
-  if [ $clean_before = YES ] ; then gmake clean ; fi
-  # CISL kills your shell if using too much resources on the login node
-  if [[ $BUILD_TARGET == cheyenne.* ]]; then
-    gmake ${MAKE_OPT} -j 3 nemsinstall
-  else
-    gmake ${MAKE_OPT} -j 8 nemsinstall
-  fi
-fi
+# ----------------------------------------------------------------------
+# Build FMS
 
+cd "$FMS_SRCDIR/fv3gfs"
+if [ $clean_before = YES ] ; then
+    $make clean
+    rm -rf "$FMS_BINDIR"
+fi
+$make ${MAKE_OPT} -j ${MAKE_THREADS} all
+
+# ----------------------------------------------------------------------
+# Build FV3
+
+cd $PATHTR
+if [ $clean_before = YES ] ; then $make clean ; fi
+$make ${MAKE_OPT} -j ${MAKE_THREADS} nemsinstall
+
+# ----------------------------------------------------------------------
 #Build NEMS
+
 cd $PATHNEMS/src
 export COMP=FV3
 export COMP_SRCDIR=$PATHTR
 export COMP_BINDIR=$PATHTR/FV3_INSTALL
-if [[ $BUILD_TARGET == "macosx.gnu" ]]; then
-  make nems COMP=,fv3, FV3_DIR=$PATHTR ${MAKE_OPT}
-else
-  gmake nems COMP=,fv3, FV3_DIR=$PATHTR ${MAKE_OPT}
-fi
+$make nems COMP=,fms,fv3, FV3_DIR=$PATHTR "FMS_DIR=$FMS_BINDIR"
 
 cp ../exe/NEMS.x ${PATHTR}/../tests/$BUILD_NAME.exe
 cp $PATHNEMS/src/conf/modules.nems ${PATHTR}/../tests/modules.$BUILD_NAME
 
+# ----------------------------------------------------------------------
+# Optionally clean
 if [ $clean_after = YES ] ; then
-  if [[ $BUILD_TARGET == "macosx.gnu" ]]; then
-    make clean
-    cd $PATHTR
-    make cleanall
-  else
-    gmake clean
-    cd $PATHTR
-    gmake cleanall
-  fi
+ $make clean
+ cd $PATHTR
+ $make cleanall
+ cd "$FMS_SRCDIR/fv3gfs"
+ $make clean
+
 # A few things that "cleanall" doesn't clean:
  rm -rf FV3_INSTALL
  rm -rf nems_dir
- find $COMP_SRCDIR/fms/ -name '*.o' -o -name '*.mod' | xargs rm -f
+ rm -rf "$FMS_BINDIR"
 fi
 
+# ------------------------------------------------------------------------
 elapsed=$SECONDS
 echo "Elapsed time $elapsed seconds. Compiling ${MAKE_OPT} finished"
 
