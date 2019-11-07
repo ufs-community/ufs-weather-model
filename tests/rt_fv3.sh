8c08151aa3f3b0d2f78babb764a581078078768a
Jessica.Meixner <Jessica.Meixner@noaa.gov>
2019-11-07 20:59:09 +0000

    Squashed commit from JessicaMeixner-NOAA/ww3update branch

--- tests/rt_fv3.sh
+++ tests/rt_fv3.sh
@@ -32,6 +32,8 @@ atparse < ${PATHTR}/parm/${INPUT_NML:-input.nml.IN} > input.nml
 
 atparse < ${PATHTR}/parm/${MODEL_CONFIGURE:-model_configure.IN} > model_configure
 
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
+
 if [[ "Q${INPUT_NEST02_NML:-}" != Q ]] ; then
     atparse < ${PATHTR}/parm/${INPUT_NEST02_NML} > input_nest02.nml
 fi
@@ -69,6 +71,8 @@ elif [[ $SCHEDULER = 'lsf' ]]; then
   fi
   atparse < $PATHRT/fv3_conf/fv3_bsub.IN > job_card
 fi
+
+atparse < ${PATHTR}/parm/${NEMS_CONFIGURE:-nems.configure} > nems.configure
 
 ################################################################################
 # Submit test
