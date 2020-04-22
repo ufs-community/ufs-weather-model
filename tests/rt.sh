06f9e531d8e7cd6e62d514f39e0f357d5587107f
Dom Heinzeller <climbfuji@ymail.com>
2020-04-22 13:34:31 -0600

    Update regression test baseline date tag

--- tests/rt.sh
+++ tests/rt.sh
@@ -375,9 +375,9 @@ while getopts ":cfsl:mkreh" opt; do
 done
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200413/${COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200422/${COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200413}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200422}
 fi
 
 shift $((OPTIND-1))
