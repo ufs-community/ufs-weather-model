ee84eab63120cd4178ae15fb76385e0c44c44fc4
Jun Wang <junwang-noaa@users.noreply.github.com>
2020-06-12 13:54:29 +0000

    udpate rt log files on cray, dell and hera

--- tests/rt.sh
+++ tests/rt.sh
@@ -378,9 +378,9 @@ while getopts ":cfsl:mkreh" opt; do
 done
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200608/${COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200611/${COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200608}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200611}
 fi
 
 shift $((OPTIND-1))
