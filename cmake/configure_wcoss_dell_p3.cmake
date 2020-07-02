b0ee9ff2dfb80699ddfd852ab58b0b19c471683a
Dom Heinzeller <climbfuji@ymail.com>
2020-07-02 06:14:43 -0600

    Update develop from gsd/develop 2020/06/30 (#156)
    - updates to GSL regression tests, add three tests to `rt.conf`, remove unnecessary regression test configuration files for GSL physics
    - turn on AVX2 flags for wcoss_dell_p3

--- cmake/configure_wcoss_dell_p3.cmake
+++ cmake/configure_wcoss_dell_p3.cmake
@@ -15,7 +15,7 @@ option(REPRO   "Enable REPRO mode" OFF)
 option(VERBOSE "Enable VERBOSE mode" OFF)
 option(32BIT   "Enable 32BIT (single precision arithmetic in dycore)" OFF)
 option(OPENMP  "Enable OpenMP threading" ON)
-option(AVX2    "Enable AVX2 instruction set" OFF)
+option(AVX2    "Enable AVX2 instruction set" ON)
 option(QUAD_PRECISION  "Enable QUAD_PRECISION (for certain grid metric terms in dycore)" ON)
 
 option(INLINE_POST "Enable inline post" ON)
