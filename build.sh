aa0763179c2234552339eb284396c3d7ae543ca7
Dusan Jovic <48258889+DusanJovic-NOAA@users.noreply.github.com>
2020-07-28 09:35:36 -0400

    Update CMakeLists.txt files to use imported targets (#172)
    Top level CMakeLists.txt and CMakeLists.txt file in FV3 have been changed (and cleaned up) to use imported targets (mostly for NCEPLIBS).
    Remove *.appBuilder compsets and log.
    Remove outdated doc directory

--- build.sh
+++ build.sh
@@ -3,17 +3,11 @@ set -eu
 
 MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
 
-export CMAKE_Platform=${CMAKE_Platform:?"Please set the CMAKE_Platform environment variable, e.g. [macosx.gnu|linux.gnu|linux.intel|hera.intel|...]"}
+#export CMAKE_Platform=${CMAKE_Platform:?"Please set the CMAKE_Platform environment variable, e.g. [macosx.gnu|linux.gnu|linux.intel|hera.intel|...]"}
 export CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-mpicc}
 export CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER:-mpicxx}
 export CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-mpif90}
 
-export BACIO_LIB4=${BACIO_LIB4:?"Please set BACIO_LIB4 environment variable"}
-export NEMSIO_INC=${NEMSIO_INC:?"Please set NEMSIO_INC environment variable"}
-export NEMSIO_LIB=${NEMSIO_LIB:?"Please set NEMSIO_LIB environment variable"}
-export SP_LIBd=${SP_LIBd:?"Please set SP_LIBd environment variable"}
-export W3EMC_LIBd=${W3EMC_LIBd:?"Please set W3EMC_LIBd environment variable"}
-export W3NCO_LIBd=${W3NCO_LIBd:?"Please set W3NCO_LIBd environment variable"}
 export NETCDF=${NETCDF:?"Please set NETCDF environment variable"}
 export ESMFMKFILE=${ESMFMKFILE:?"Please set ESMFMKFILE environment variable"}
 
@@ -22,7 +16,8 @@ rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 
 CCPP_SUITES="${CCPP_SUITES:-FV3_GFS_2017_gfdlmp}"
-CMAKE_FLAGS+=" -DCCPP=ON -DSUITES=${CCPP_SUITES} -DNETCDF_DIR=${NETCDF}"
+CMAKE_FLAGS+=" -DCCPP_SUITES=${CCPP_SUITES} -DNETCDF_DIR=${NETCDF}"
+CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Debug -DDEBUG=ON"
 
 cd ${BUILD_DIR}
 cmake .. ${CMAKE_FLAGS}
