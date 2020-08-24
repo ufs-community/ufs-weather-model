6fac442142b712ad2603a582c1e7766340282723
Jun Wang <junwang-noaa@users.noreply.github.com>
2020-08-24 01:35:35 +0000

    update log files from hera

--- tests/rt.sh
+++ tests/rt.sh
@@ -412,9 +412,9 @@ if [[ $SINGLE_NAME != '' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200812/${COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200821/${COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200812}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20200821}
 fi
 
 shift $((OPTIND-1))
