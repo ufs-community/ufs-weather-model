407df4ecebebe43550859d5ec6f2b8155dc9ea1f
Minsuk Ji <57227195+MinsukJi-NOAA@users.noreply.github.com>
2020-09-09 10:19:04 -0400

    github actions utest (#169)
    * add ci-related files. update utest
    * modify to auto-run different cases based on ci.test
    * Improve CI workflow
    * Separate builds and tests into different jobs
    * Separate input data from build image; instead, use it as a volume before running tests
    * Make ci subdirectory to contain ci-related files
    * add clean-up after utest run
    * add workflow manage files
    * move parsing in main.yml to a separate script file

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -35,6 +35,17 @@ elif [[ $MACHINE_ID = hera.* ]]; then
   TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
   TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
 
+elif [[ $MACHINE_ID = linux.* ]]; then
+
+  if [[ $CI_TEST = true ]]; then
+  TASKS_dflt=12 ; TPN_dflt=16 ; INPES_dflt=1 ; JNPES_dflt=1
+  else
+  TASKS_dflt=150 ; TPN_dflt=40 ; INPES_dflt=3 ; JNPES_dflt=8
+  fi
+  TASKS_thrd=84  ; TPN_thrd=20 ; INPES_thrd=3 ; JNPES_thrd=4
+  TASKS_stretch=48 ; TPN_stretch=12 ; INPES_stretch=2 ; JNPES_stretch=4
+  TASKS_strnest=96 ; TPN_strnest=12 ; INPES_strnest=2 ; JNPES_strnest=4
+
 elif [[ $MACHINE_ID = jet.* ]]; then
 
   TASKS_dflt=150 ; TPN_dflt=24 ; INPES_dflt=3 ; JNPES_dflt=8
