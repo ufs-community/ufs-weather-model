429644dce8da036c963996ac801640f0a143f1b5
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-10-19 14:53:58 -0400

    update baseline date-tag

--- tests/rt.sh
+++ tests/rt.sh
@@ -400,9 +400,9 @@ if [[ $SINGLE_NAME != '' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = jet.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201015/${RT_COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201019/${RT_COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201015}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201019}
 fi
 
 shift $((OPTIND-1))
