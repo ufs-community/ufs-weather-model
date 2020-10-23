4694a1df11bda9e75eaa48f89887ac69e2499293
denise.worthen <Denise.Worthen@noaa.gov>
2020-10-23 09:25:39 +0000

    point to new baseline skip-ci

--- tests/rt.sh
+++ tests/rt.sh
@@ -400,7 +400,7 @@ if [[ $SINGLE_NAME != '' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = jet.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201019/${RT_COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201022/${RT_COMPILER^^}}
 else
   RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201019}
 fi
