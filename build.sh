2106f6403bc871101302a7ccae48be5b0a71df87
Dom Heinzeller <climbfuji@ymail.com>
2020-10-26 19:36:57 -0600

    Move clang test for turning off OpenMP from cmake/configure_macosx.gnu.cmake to CMakeLists.txt so that it works with build.sh; update build.sh to usee correct readlink command on macOS

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eu
 
-MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+if [[ $(uname -s) == Darwin ]]; then
+  readonly MYDIR=$(cd "$(dirname "$(greadlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+else
+  readonly MYDIR=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )" && pwd -P)
+fi
 
 export CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-mpicc}
 export CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER:-mpicxx}
@@ -14,7 +18,7 @@ BUILD_DIR=${MYDIR}/build
 rm -rf ${BUILD_DIR}
 mkdir ${BUILD_DIR}
 
-CCPP_SUITES="${CCPP_SUITES:-FV3_GFS_v15p2}"
+CCPP_SUITES="${CCPP_SUITES:-FV3_GFS_v15p2,FV3_RRFS_v1beta}"
 CMAKE_FLAGS+=" -DCCPP_SUITES=${CCPP_SUITES} -DNETCDF_DIR=${NETCDF}"
 
 cd ${BUILD_DIR}
