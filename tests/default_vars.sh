ca288f44ac11567173d3cd905d96084f20542d7b
denise.worthen <Denise.Worthen@noaa.gov>
2020-10-28 12:47:19 -0400

    update cmeps
    use updcmeps branch of CMEPS-interface/CMEPS
    add file to CMakeLists
    shorten name of nems.configure file for coupled model
    clean up white space

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -365,7 +365,7 @@ export CPL_SLOW=${DT_THERM_MOM6}
 export CPL_FAST=${DT_ATMOS}
 
 # nems.configure defaults
-export NEMS_CONFIGURE="nems.configure.medcmeps_atm_ocn_ice.IN"
+export NEMS_CONFIGURE="nems.configure.cpld.IN"
 export med_model="nems"
 export atm_model="fv3"
 export ocn_model="mom6"
@@ -418,9 +418,9 @@ export MOM6_RESTART_SETTING='n'
 export MOM6_RIVER_RUNOFF='False'
 export FRUNOFF=""
 export CHLCLIM="seawifs_1998-2006_smoothed_2X.nc"
-# this must be set False for restart repro 
+# this must be set False for restart repro
 export MOM6_REPRO_LA='False'
-# since CPL_SLOW is set to DT_THERM, this should be always be false 
+# since CPL_SLOW is set to DT_THERM, this should be always be false
 export MOM6_THERMO_SPAN='False'
 # no WW3
 export MOM6_USE_WAVES='False'
