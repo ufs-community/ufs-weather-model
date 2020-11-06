2aff1cee2c575d45e409afbedfeb7f88b0e5872d
denise.worthen <Denise.Worthen@noaa.gov>
2020-11-06 18:36:25 +0000

    add wcoss-cray logs
    update baseline to develop-20201106; skip-ci

--- tests/rt.sh
+++ tests/rt.sh
@@ -400,9 +400,9 @@ if [[ $SINGLE_NAME != '' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = jet.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201103/${RT_COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201106/${RT_COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201103}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201106}
 fi
 
 shift $((OPTIND-1))
