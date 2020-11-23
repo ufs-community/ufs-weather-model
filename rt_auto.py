9d93b8d10bd63fc3d06395f9c1a27e631bf29536
BuildTools <unconfigured@null.spigotmc.org>
2020-11-23 10:33:12 -0500

    rt automation script w/o token

--- rt_auto.py
+++ rt_auto.py
@@ -4,10 +4,18 @@
 
 from github import Github as gh
 
+USERNAME = None
+PASSWORD = None
+
+def connect_to_github(): # In case there's more needed later
+    global USERNAME
+    global PASSWORD
+    client = gh(USERNAME, PASSWORD) #essentially
+    return client
+
 # Initial items
-g = gh("05630912c8d01499a0135f760f01b96f0f6fe127")
-user = g.get_user()
-repo = g.get_repo("BrianCurtis-NOAA/ufs-weather-model")
+client = connect_to_github()
+repo = client.get_repo("BrianCurtis-NOAA/ufs-weather-model")
 print(repo.name)
 
 pulls = repo.get_pulls(state='open', sort='created', base='develop')
