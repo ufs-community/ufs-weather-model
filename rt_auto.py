cff844619c498e338fc54026b6f428163e2cba32
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 16:43:04 -0500

    Updates to python script, and addition of YAML database

--- rt_auto.py
+++ rt_auto.py
@@ -2,44 +2,78 @@
 # Written by Brian Curtis
 # Automation of RT tasks using github cli
 
-#from github import Github as gh
+from github import Github as gh
+import pandas as pd
 import socket
 import re
 import sys
+import yaml
 
-USERNAME = None
-PASSWORD = None
+GHACCESSTOKEN = None
 
 def get_machine_name():
     hostname = socket.gethostname()
-#    hfe##  # HERA
-    if re.match()
-#    v##X#  # VENUS
-#    m##X#  # MARS
-#    slogin# # SURGE
-#    Orion-login-3.HPC.MsState.Edu #ORION
-    print(re.match("hfe\d\d", hostname))
-    print(re.match("N[a-z][a-z][a-z]", hostname))
+    machines = read_yaml_data("machines")
+    df = pd.DataFrame.from_dict(machines)
+    A = df['name'][df.index[df.regexhostname.str.match(hostname)].tolist()]
+    if len(A) == 1:
+        machine = list(A)[0]
+        print("Approved Machine Found: {}".format(machine))
+        return machine
+    else:
+        sys.exit("Hostname {} does not match approved list. Quitting".format(hostname))
+
+def is_user_approved(userin):
+    approvedusers = read_yaml_data('approvedusers')
+    userlist = pd.DataFrame.from_dict(approvedusers)
+    A = userlist['username'][userlist.index[userlist.username.str.match(userin)].tolist()]
+    if len(A) == 1:
+        user = list(A)[0]
+        print("Approved Github Username Found: {}".format(user))
+        return user
+    else:
+        sys.exit("Username {} does not match approved list. Quitting".format(hostname))
+
+def get_repos_list():
+    repos = read_yaml_data("repository")
+    df = pd.DataFrame.from_dict(repos)
+    return df
 
 def connect_to_github(): # In case there's more needed later
-    global USERNAME
-    global PASSWORD
-    client = gh(USERNAME, PASSWORD) #essentially
+    global GHACCESSTOKEN
+    client = gh(GHACCESSTOKEN) #will work if None for public repos.
     return client
 
-# Initial items
-get_machine_name()
-sys.exit()
-client = connect_to_github()
-repo = client.get_repo("BrianCurtis-NOAA/ufs-weather-model")
-print(repo.name)
+def read_yaml_data(keyin):
+    stream = open("rt_auto.yml", 'r')
+    dictionary = yaml.load(stream, Loader=yaml.SafeLoader)
+    try:
+        dataout = dictionary[keyin]
+    except KeyError as e:
+        sys.exit("No data available with name {}. Quitting".format(keyin))
+    return dataout
 
-pulls = repo.get_pulls(state='open', sort='created', base='develop')
-for pr in pulls:
-    print(pr.number)
-    comments = pr.get_issue_comments()
-    for comment in comments:
-        print(comment.body)
+
+# Initial items
+machine = get_machine_name() #ALWAYS RUN THIS FIRST
+repos = get_repos_list()
+client = connect_to_github()
+for name,address,base in repos.values.tolist():
+    repo = client.get_repo(address)
+    pulls = repo.get_pulls(state='open', sort='created', base='develop')
+    for pr in pulls:
+        print(pr.head.repo.default_branch)
+        print(pr.head.repo.master_branch)
+        print(pr.number)
+        comments = pr.get_issue_comments()
+        for comment in comments:
+            comment_user = comment.user.login
+            print("comment user: {}".format(comment_user))
+            is_user_approved(comment_user)
+            print(comment.body)
+
+
+
 
 # CHECK IS USER ON APPROVED LIST
 
