dc6348c712680daa16aa980bb5ef23468c060312
Dom Heinzeller <climbfuji@ymail.com>
2020-12-01 10:53:32 -0700

    Update regression test baseline date tag to 20201201; skip-ci

--- tests/rt.sh
+++ tests/rt.sh
@@ -396,9 +396,9 @@ if [[ $TESTS_FILE =~ '35d' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = gaea.* ]] || [[ $MACHINE_ID = jet.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201127/${RT_COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201201/${RT_COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201127}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20201201}
 fi
 
 shift $((OPTIND-1))
