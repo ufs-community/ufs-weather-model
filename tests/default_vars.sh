e74cefeaf1d5e0bff0b2279fa88b9dc5776f9ad1
denise.worthen <Denise.Worthen@noaa.gov>
2020-12-12 09:37:31 -0600

    revert dt_cice to 900s (error in #897f66c); skip-ci

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -576,7 +576,7 @@ export CDATE=${SYEAR}${SMONTH}${SDAY}${SHOUR}
 
 export NFHOUT=6
 export FDIAG=6
-export DT_ATMOS='3600'
+export DT_ATMOS='900'
 export DT_CICE=${DT_ATMOS}
 export DT_DYNAM_MOM6='1800'
 export DT_THERM_MOM6='3600'
