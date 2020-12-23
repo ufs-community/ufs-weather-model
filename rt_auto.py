cc57e4545a614cd2faea2fb103db6ebf33724afa
Brian Curtis <Brian.Curtis@noaa.gov>
2020-12-22 21:12:03 -0500

    Switched to using Labels

--- rt_auto.py
+++ rt_auto.py
@@ -14,6 +14,7 @@ import yaml
 import os
 
 # WARNING WARNING WARNING: DO NOT COMMIT WITH THE GHACCESSTOKEN
+GHACCESSTOKEN = "e670110df066c9ba2ceeb8042103255ee1fba4fb"
 # GHACCESSTOKEN = None
 # GITHUB RATE LIMIT FOR AUTHENTICATED USERS IS 5000 REQUESTS PER HOUR
 db_filename = 'rt_auto.yml'
@@ -23,12 +24,11 @@ machine = None
 
 class machine_info():
 
-    def __init__(self, name, regexhostname, workdir):
+    def __init__(self, name, regexhostname, workdir, baselinedir):
         self.name = name
         self.regexhostname = regexhostname
         self.workdir = workdir
-
-
+        self.baselinedir = baselinedir
 
 def get_machine_info(static_data):
     hostname = socket.gethostname()
@@ -41,20 +41,9 @@ def get_machine_info(static_data):
         sys.exit("Hostname {} does not match approved list. Quitting".format(hostname))
 
     mach_db_info = machines.loc[machines[machines['name'] == app_machine_name].index.values[0]]
-    machine = machine_info(mach_db_info['name'], mach_db_info['regexhostname'], mach_db_info['workdir'])
+    machine = machine_info(mach_db_info['name'], mach_db_info['regexhostname'],
+                           mach_db_info['workdir'], mach_db_info['baselinedir'])
     return machine
-
-# THE FOLLOWING FUNCTION REQUIRES AUTHENTICATION TOKEN TO BE SET
-def is_collaborator(repo, userin):
-    collaborator_bool = False
-    allowed_collaborators = repo.get_collaborators()
-    for collaborator in allowed_collaborators:
-        collab_perms = repo.get_collaborator_permission(collaborator)
-        print("Collab {}, Perms {}".format(collaborator.login, collab_perms))
-        if collaborator.login == userin && 'write' in collab_perms.split():
-            collaborator_bool = True
-            
-    return collaborator_bool
 
 def get_yaml_subset(in_yaml, keyin):
     try:
@@ -75,90 +64,18 @@ def read_yaml_data(filename):
     stream.close()
     return yaml_data
 
-# def process_triggers(pr, triggers, commentids):
-def process_triggers(comments_with_triggers, pr, repo):
-    global machine
-    if not machine:
-        sys.exit("No machine information set, please use \"get_machine_info()\"")
-    lastread = False
-    approved_triggers = []
-    trigger_comment_ids = []
-    for comment in comments_with_triggers:
-        body_split = comment.body.split()
-        for word in body_split:
-            if re.match("\+\+.+\+\+", word):
-                if is_collaborator(repo, comment.user.login):
-                    split_trigger = str(word).strip("+")
-                    split_trigger = split_trigger.split(":")
-                    if split_trigger[0].lower() == "read":
-                        if bool(lastread):
-                            print("Found old 'READ' comment: deleting")
-                            pr.get_issue_comment(int(lastread)).delete()
-                        lastread = comment.id
-                    else:
-                        approved_triggers.append(split_trigger)
-                        trigger_comment_ids.append(comment.id)
-
-    actions = []
-    for (approved_trigger, trigger_comment_id) in zip(approved_triggers, trigger_comment_ids):
-        if approved_trigger[0].lower() == machine.name.lower() or approved_trigger[0].lower() == "all":
-            if trigger_comment_id > lastread:
-                actions.append(approved_trigger[1])
-                print("SAVED ACTION {} for machine {}".format(approved_trigger[1], machine.name))
-
-    if not actions:
-        return lastread, None
-    actions = set(actions)
-    return lastread, actions
-
-def get_comments_with_triggers(comments):
-    comment_list = []
-    for comment in comments:
-        if [re.match("\+\+.+\+\+", incomment) for incomment in comment.body.split()]:
-            comment_list.append(comment)
-
-    if not comment_list:
-        return None
-    else:
-        return comment_list
-
 def process_pulls(pulls, repo):
-    for pr in pulls:
-        comments = pr.get_issue_comments()
-        comments_with_triggers = get_comments_with_triggers(comments)
-        if not comments_with_triggers:
-            # LETS STOP HERE AND UPDATE THE 'READ' TRIGGER
-            # ASSUMING THERE'S ZERO 'READ' COMMENTS, ADDING ONE AT END
-            pr.create_issue_comment("++READ++")
-            continue
-
-        lastread, actions = process_triggers(comments_with_triggers, pr, repo)
-        if bool(lastread):
-            if lastread != max(comment.id for comment in comments):
-                pr.get_issue_comment(int(lastread)).delete()
-                print("Deleted previous READ comment")
-                pr.create_issue_comment("++READ++")
-
-
-        if bool(actions):
-            print("Accepted actions: {}".format(actions))
-            validate_actions(actions)
-            pr_workdir = clone_pr_repo(pr)
-            process_actions(actions, pr_workdir, pr)
-        else:
-            print("No Actions Found in this PR, skipping.")
-
-def validate_actions(actions):
-    approved_commands = []
+    global machine
     valid_actions = get_yaml_subset(static_data, "actions")
-    for name,command in valid_actions.values.tolist():
-        for action in actions:
-            if name == action.lower():
-                print("Action {} approved as valid".format(action.lower()))
-                approved_commands.append(command)
-    return approved_commands
-
-
+    for pr in pulls:
+        labels = pr.get_labels()
+        for label in labels:
+            if label.name.split('-')[1].lower() == machine.name.lower():
+                for action_name, action_command in valid_actions.values.tolist():
+                    if label.name.split('-')[0].lower() == action_name.lower():
+                        pr_workdir = clone_pr_repo(pr)
+                        process_actions(action_command, pr_workdir, pr)
+        sys.exit()
 
 def clone_pr_repo(pr):
     global machine
@@ -202,20 +119,20 @@ def create_threaded_call(callback, run_fnc):
 
     return thread # returns immediately after the thread starts
 
+def callback_commands(argument):
+    callbacks = {
+        'RT' =
+    }
+
 def thread_end(pr, command, pr_workdir):
     global machine
     pr.create_issue_comment("AUTOMATED COMMENT: Finished Processing Command '{}' in directory '{}' on machine '{}'".format(command, pr_workdir, machine.name))
     return
 
-def process_actions(actions, pr_workdir, pr):
-    global machine
-
-    approved_commands = validate_actions(actions)
-    if bool(approved_commands):
-        for command in approved_commands:
-            print("{} is processing command {}".format(machine.name.upper(), command))
-            pr.create_issue_comment("AUTOMATED COMMENT: Started Processing Command '{}' in directory '{}' on machine '{}'".format(command, pr_workdir, machine.name))
-            create_threaded_call(thread_end(pr, command, pr_workdir+"/tests"), subprocess.Popen(command, shell=True, cwd=pr_workdir+"/tests"))
+def process_actions(command, pr_workdir, pr):
+        print("{} is processing command {}".format(machine.name.upper(), command))
+        pr.create_issue_comment("AUTOMATED COMMENT: Started Processing Command '{}' in directory '{}' on machine '{}'".format(command, pr_workdir, machine.name))
+        create_threaded_call(thread_end(pr, command, pr_workdir+"/tests"), subprocess.Popen(command, shell=True, cwd=pr_workdir+"/tests"))
 
 # Initial items
 static_data = read_yaml_data('rt_auto.yml')
@@ -228,3 +145,74 @@ for name,address,base in repos.values.tolist():
     repo = client.get_repo(address)
     pull_reqs = repo.get_pulls(state='open', sort='created', base=base)
     triggers = process_pulls(pull_reqs, repo)
+
+
+# DEAD FUNCTIONS
+# def process_triggers(pr, triggers, commentids):
+# def process_triggers(comments_with_triggers, pr, repo):
+#     global machine
+#     if not machine:
+#         sys.exit("No machine information set, please use \"get_machine_info()\"")
+#     lastread = False
+#     approved_triggers = []
+#     trigger_comment_ids = []
+#     for comment in comments_with_triggers:
+#         body_split = comment.body.split()
+#         for word in body_split:
+#             if re.match("\+\+.+\+\+", word):
+#                 if is_collaborator(repo, comment.user.login):
+#                     split_trigger = str(word).strip("+")
+#                     split_trigger = split_trigger.split(":")
+#                     if split_trigger[0].lower() == "read":
+#                         if bool(lastread):
+#                             print("Found old 'READ' comment: deleting")
+#                             pr.get_issue_comment(int(lastread)).delete()
+#                         lastread = comment.id
+#                     else:
+#                         approved_triggers.append(split_trigger)
+#                         trigger_comment_ids.append(comment.id)
+#
+#     actions = []
+#     for (approved_trigger, trigger_comment_id) in zip(approved_triggers, trigger_comment_ids):
+#         if approved_trigger[0].lower() == machine.name.lower() or approved_trigger[0].lower() == "all":
+#             if trigger_comment_id > lastread:
+#                 actions.append(approved_trigger[1])
+#                 print("SAVED ACTION {} for machine {}".format(approved_trigger[1], machine.name))
+#
+#     if not actions:
+#         return lastread, None
+#     actions = set(actions)
+#     return lastread, actions
+#
+# def get_comments_with_triggers(comments):
+#     comment_list = []
+#     for comment in comments:
+#         if [re.match("\+\+.+\+\+", incomment) for incomment in comment.body.split()]:
+#             comment_list.append(comment)
+#
+#     if not comment_list:
+#         return None
+#     else:
+#         return comment_list
+#
+# # THE FOLLOWING FUNCTION REQUIRES AUTHENTICATION TOKEN TO BE SET
+# def is_collaborator(repo, userin):
+#     collaborator_bool = False
+#     allowed_collaborators = repo.get_collaborators()
+#     for collaborator in allowed_collaborators:
+#         collab_perms = repo.get_collaborator_permission(collaborator)
+#         print("Collab {}, Perms {}".format(collaborator.login, collab_perms))
+#         if collaborator.login == userin && 'write' in collab_perms.split():
+#             collaborator_bool = True
+#
+#     return collaborator_bool
+#
+# def validate_actions(actions):
+#     approved_commands = []
+#     valid_actions = get_yaml_subset(static_data, "actions")
+#     for name,command in valid_actions.values.tolist():
+#         for action in actions:
+#             if name == action.lower():
+#                 print("Action {} approved as valid".format(action.lower()))
+#                 approved_commands.append(command)
+#     return approved_commands
