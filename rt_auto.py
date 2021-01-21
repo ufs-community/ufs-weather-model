baea07d1c70ecbb590d16e5cae9b59c539b4c289
Brian Curtis <brian.curtis@noaa.gov>
2021-01-21 21:20:54 +0000

    Moved towards OO style, tested on hera successfully

--- rt_auto.py
+++ rt_auto.py
@@ -38,6 +38,7 @@ class Machine:
 
     def __init__(self, RTData):
         self.RTData = RTData
+        self.get_machine_info()
 
     def get_machine_info(self):
         hostname = socket.gethostname()
@@ -51,65 +52,29 @@ class Machine:
                 self.regexhostname = mach_db_info['regexhostname']
                 self.workdir = mach_db_info['workdir']
                 self.baselinedir = mach_db_info['baselinedir']
-                break
+                return
             else:
                 continue
         sys.exit(f'Hostname:{hostname} does not match approved list. Quitting')
 
-class Repos:
+class Function:
 
-    def __init__(self, ghinterface, rtdata):
-        self.client = ghinterface.client
-        self.rtdata = rtdata
-        self.repo_info()
+    def __init__(self, name, command, callback):
+        self.name = name
+        self.command = command
+        self.callback = callback
 
-    class RepoInfo:
-        def __init__(self, name, address, base):
-            self.name = name
-            self.address = address
-            self.base = base
+    def verify_name(self, comparable):
+        if re.match(self.name.lower(), comparable.lower()):
+            return True
+        else:
+            return False
 
-    def repo_info(self):
-        repo_data = self.rtdata.get_yaml_subset('repository').\
-            values.tolist()
-        repo_list = [self.RepoInfo(name,address,base)
-            for name,address,base in repo_data]
-        print(f'type repo_list is {type(repo_list)}')
-        if not isinstance(repo_list, list):
-            repo_list = list(repo_list)
-        self.repo_list = repo_list
-
-class ActionInfo:
-
-    def __init__(self, rtdata):
-        self.rtdata = rtdata
-        self.action_info()
-
-    class ActionData:
-        def __init__(self, name, command, callback_fnc):
-            self.name = name
-            self.command = command
-            self.callback_fnc = callback_fnc
-
-    def action_info(self):
-        action_data = self.rtdata.get_yaml_subset('actions').\
-            values.tolist()
-        self.action_list = [self.ActionData(name,command,callback_fnc)
-            for name,command,callback_fnc in action_data]
-
-    def is_action_name(self, action_name_cmp):
-        for match_action in self.action_list:
-            if re.match(action_name_cmp.lower(), match_action.name.lower()):
-                return True
-            else:
-                return False
-
-    def get_action(self, action_name_cmp):
-        for match_action in self.action_list:
-            if re.match(action_name_cmp.lower(), match_action.name.lower()):
-                return match_action
-            else:
-                return None
+    def verify_command(self, comparable):
+        if re.match(self.command.lower(), comparable.lower()):
+            return True
+        else:
+            return False
 
 class GHInterface:
 
@@ -125,76 +90,147 @@ class GHInterface:
             f.close()
             self.GHACCESSTOKEN = self.GHACCESSTOKEN.decode('utf-8').strip('\n')
         else:
-            sys.exit('Please create a file "accesstoken.txt" that contains your\
-                GitHub API Token.\nMake sure to set permissions so others can\
-                not read it (400)')
+            sys.exit('Please create a file "accesstoken.txt" that contains your'\
+                ' GitHub API Token.\nMake sure to set permissions so others can'\
+                ' not read it (400)')
 
-class ProcessRepo:
+# REPO STUFF
+class Repo:
 
-    def __init__(self, machine, ghinterface, actions, repo):
+    def __init__(self, name, address, base, machine, ghinterface):
+        self.name = name
+        self.address = address
+        self.base = base
         self.machine = machine
         self.ghinterface = ghinterface
-        self.actions = actions
-        self.repo = repo
-        self.get_repo_object()
+        self.get_GHrepo_object()
         self.get_repo_preqs()
 
-    def get_repo_object(self):
+    def get_GHrepo_object(self):
         try:
-            self.ghrepo = self.ghinterface.client.get_repo(self.repo.address)
+            self.ghrepo = self.ghinterface.client.get_repo(self.address)
         except Exception as e:
             print(f'Failed to get repo object with error {e}')
             self.ghrepo = None
 
     def get_repo_preqs(self):
-        self.preq_list = []
+        self.pullreq_list = []
         try:
-            preqs = self.ghrepo.get_pulls(state='open', sort='created', base=self.repo.base)
+            preqs = self.ghrepo.get_pulls(state='open', sort='created', base=self.base)
         except Exception as e:
             print(f'Failed to get pull object with error {e}')
             preqs = None
 
         for preq in preqs:
-            pullreq = PullReq(preq)
-            sys.exit()
-            pullreq.process()
-            # self.preq_list.append(self.PullReq(preq))
+            self.pullreq_list.append(PullReq(self, preq, self.machine))
 
-class PullReq(ProcessRepo):
+class PullReq:
 
-    def __init__(self, preq):
-        super(PullReq, self).__init__()
-        print(f'MACHINE NAMEMEMEME {super().machine.name}')
+    def __init__(self, Repo, preq, machine):
         self.preq = preq
+        self.repo = Repo
+        self.machine = machine
+
         self.branch = self.preq.head.ref
         self.repo_name = self.preq.head.repo.name
         self.git_url = self.preq.head.repo.html_url
-        self.repo_dir_str = super().machine.workdir+'/'+str(pr.id)+'/'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        self.get_labels()
+        self.repo_dir_str = self.machine.workdir+'/'+str(preq.id)+'/'+datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        self.get_pr_labels()
 
-
-
-    def get_labels(self):
-        self.prlabels = []
-        pr_labels = super().preq.get_labels()
+    def get_pr_labels(self):
+        self.labels = []
+        pr_labels = self.preq.get_labels()
         for pr_label in pr_labels:
             split_pr_label = pr_label.name.split('-')
-            self.prlabels.append(PRLabel(split_pr_label[0], split_pr_label[1]))
+            self.labels.append(PRLabel(split_pr_label[0], split_pr_label[1]))
 
-    def clone_pr_repo(self):
+    def add_clone_dir(self, clone_dir):
+        self.clone_dir = clone_dir
 
-        git_url_w_login = self.git_url.split('//')
-        git_url_w_login = git_url_w_login[0]+"//"+super().ghinterface.GHUSERNAME+":"+super().ghinterface.GHACCESSTOKEN+"@"+git_url_w_login[1]
+class PRLabel:
 
-        create_repo_commands = [
-            ['mkdir -p "'+self.repo_dir_str+'"', super().machine.workdir],
-            ['git clone -b '+self.branch+' '+git_url_w_login, self.repo_dir_str],
-            ['git submodule update --init --recursive', self.repo_dir_str+'/'+self.repo_name],
-            ['module use modulefiles/{}.intel && module load fv3'.format(super().machine.name.lower()), self.repo_dir_str+'/'+self.repo_name]
+    def __init__(self, name, machine):
+        self.name = name
+        self.machine = machine
+        self.function = None
+
+    def add_function(self, function):
+        self.function = function
+
+    def is_approved(self, machine, functions):
+        if re.match(self.machine.lower(), machine.name.lower()):
+            for function in functions:
+                if function.verify_name(self.name):
+                    print(f'Approved label "{self.name}-{self.machine}"')
+                    self.add_function(function)
+                    return True
+                else:
+                    print(f'Label not approved. Name: {self.name} not on approved list.')
+                    return False
+        else:
+            print(f'Label not approved. Machine: {self.machine} not on approved list.')
+            return False
+
+def get_approved_functions(rtdata):
+    function_data = rtdata.get_yaml_subset('functions').\
+        values.tolist()
+    function_list = [Function(name,command,callback_fnc)
+        for name,command,callback_fnc in function_data]
+    return function_list
+
+def clone_pr_repo(pullreq, ghinterface, machine):
+
+    git_url_w_login = pullreq.git_url.split('//')
+    git_url_w_login = git_url_w_login[0]+"//"+ghinterface.GHUSERNAME+":"+ghinterface.GHACCESSTOKEN+"@"+git_url_w_login[1]
+
+    create_repo_commands = [
+        ['mkdir -p "'+pullreq.repo_dir_str+'"', machine.workdir],
+        ['git clone -b '+pullreq.branch+' '+git_url_w_login, pullreq.repo_dir_str],
+        ['git submodule update --init --recursive', pullreq.repo_dir_str+'/'+pullreq.repo_name],
+        ['module use modulefiles/{}.intel && module load fv3'.format(machine.name.lower()), pullreq.repo_dir_str+'/'+pullreq.repo_name]
+    ]
+
+    for command, in_cwd in create_repo_commands:
+        print(f'Attempting to run: {command}'.format(command))
+        try:
+            retcode = subprocess.Popen(command, shell=True, cwd=in_cwd)
+            retcode.wait()
+            if retcode.returncode==1:
+                print('Error Occured:')
+                print(f'Stdout: {retcode.stdout}')
+                print(f'Stderr: {retcode.stderr}')
+                sys.exit()
+        except OSError as e:
+            print(f'Execution failed: {e}')
+            sys.exit()
+
+    pullreq.add_clone_dir(pullreq.repo_dir_str+"/"+pullreq.repo_name)
+
+def process_pr(pullreq, ghinterface, machine, functions):
+    for prlabel in pullreq.labels:
+        if prlabel.is_approved(machine, functions):
+            try:
+                clone_pr_repo(pullreq, ghinterface, machine)
+            except Exception as e:
+                sys.exit(f'Error cloning repo: {pullreq.git_url}')
+            try:
+                send_to_thread(prlabel.function, pullreq)
+            except Exception as e:
+                print(f'ERROR RUNNING RT {prlabel.function.command} with error: {e}')
+                continue
+            # pullreq.preq.remove_from_labels(f'{prlabel.name}-{prlabel.machine}')
+
+def move_rt_logs(PullReq):
+    filepath = PullReq.clone_dir+'/'+rt_log
+    if os.path.exists(filepath):
+
+        move_rt_commands = [
+            ['git add '+rt_log, PullReq.clone_dir],
+            ['git commit -m "Auto: Added Updated RT Log file: '+rt_log+'"', PullReq.clone_dir],
+            ['git push origin '+PullReq.branch, PullReq.clone_dir]
         ]
-
-        for command, in_cwd in create_repo_commands:
-            print(f'Attempting to run: {command}'.format(command))
+        for command, in_cwd in move_rt_commands:
+            print(f'Attempting to run: {command}')
             try:
                 retcode = subprocess.Popen(command, shell=True, cwd=in_cwd)
                 retcode.wait()
@@ -206,103 +242,48 @@ class PullReq(ProcessRepo):
             except OSError as e:
                 print(f'Execution failed: {e}')
                 sys.exit()
+    else:
+        print('ERROR: Could not find RT log')
+        sys.exit()
 
-        self.clone_location = self.repo_dir_str+"/"+self.repo_name
+def send_to_thread(Function, PullReq):
 
-    def process(self):
-        for prlabel in self.prlabels:
-            if prlabel.is_approved():
-                self.approved_action = self.actions.get_action(prlabel.command)
-            try:
-                self.clone_pr_repo()
-            except Exception as e:
-                sys.exit(f'Error cloning repo: {self.git_url}')
-            try:
-                self.process_actions(self.approved_action.callback_fnc, self.approved_action.command, self.clone_location)
-            except Exception as e:
-                print(f'ERROR RUNNING RT {self.approved_action.command} with error: {e}')
-                continue
-            self.preq.remove_from_labels(f'{prlabel.command}-{super().machine.name}')
+    def create_threaded_call(Function, PullReq):
 
-    def move_rt_logs(self):
-        rt_log = 'tests/RegressionTests_'+super().machine.name+'.intel.log'
-        filepath = self.clone_location+'/'+rt_log
-        print(f'File path issssss {filepath}')
-        if os.path.exists(filepath):
-            print(f'Branch used is: {self.branch}')
+        def runInThread(incallback, incommand, cwd_in):
+            print(f'cwd_in is:{cwd_in}')
+            proc = subprocess.Popen(incommand, shell=True, cwd=cwd_in)
+            proc.wait()
+            globals()[incallback](PullReq)
+            return
 
-            move_rt_commands = [
-                ['git add '+rt_log, self.clone_location],
-                ['git commit -m "Auto: Added Updated RT Log file: '+rt_log+'"', self.clone_location],
-                ['git push origin '+branch, self.clone_location]
-            ]
-            for command, in_cwd in move_rt_commands:
-                print(f'Attempting to run: {command}')
-                try:
-                    retcode = subprocess.Popen(command, shell=True, cwd=in_cwd)
-                    retcode.wait()
-                    if retcode.returncode==1:
-                        print('Error Occured:')
-                        print(f'Stdout: {retcode.stdout}')
-                        print(f'Stderr: {retcode.stderr}')
-                        sys.exit()
-                except OSError as e:
-                    print(f'Execution failed: {e}')
-                    sys.exit()
-        else:
-            print('ERROR: Could not find RT log')
-            sys.exit()
+        thread = threading.Thread(target=runInThread,
+                                  args=(Function.callback, Function.command, PullReq.clone_dir))
+        # thread.daemon=True
+        thread.start()
 
-    def process_actions(callback_fnc, command):
+        return thread # returns immediately after the thread starts
+    thread = create_threaded_call(Function, PullReq)
 
-        def create_threaded_call(callback_fnc, command, cwd_in):
-
-            def runInThread(callback_fnc, command, cwd_in):
-                proc = subprocess.Popen(command, shell=True, cwd=cwd_in)
-                proc.wait()
-                globals()[callback_fnc](self)
-                return
-
-            thread = threading.Thread(target=runInThread,
-                                      args=(callback_fnc, command, cwd_in))
-            thread.daemon=True
-            thread.start()
-
-            return thread # returns immediately after the thread starts
-
-        print(f'{machine.name.upper()} is running command {command}')
-        thread = create_threaded_call(callback_fnc, command, self.clone_location+'/tests')
-
-class PRLabel(PullReq):
-    def __init__(self, command, machine):
-        self.lblcommand = command
-        self.lblmachine = machine
-
-    def is_approved(self):
-        if re.match(machine.lower(), super().machine.name.lower()):
-            if super().actions.is_action_name(command):
-                print(f'Approved label "{command}-{machine}"')
-                return True
-            else:
-                print(f'Label not approved. Command: {command} not on approved list.')
-                return False
-        else:
-            print(f'Label not approved. Machine: {machine} does not match.')
-            return False
+def get_approved_repos(rtdata, machine, ghinterface):
+    repo_data = rtdata.get_yaml_subset('repository').values.tolist()
+    repo_list = [Repo(name, address, base, machine, ghinterface)
+        for name, address, base in repo_data]
+    if not isinstance(repo_list, list):
+        repo_list = list(repo_list)
+    return repo_list
 
 def main():
     GHUSERNAME = 'BrianCurtis-NOAA'
-
+    ghinterface = GHInterface(GHUSERNAME)
     rtdata = RTData()
     machine = Machine(rtdata)
-    actions = ActionInfo(rtdata)
-    # repo_list = get_repo_info(rtdata)
-    ghinterface = GHInterface(GHUSERNAME)
-    repos = Repos(ghinterface, rtdata)
-    print(f'type of repos is {type(repos)}')
-    # repo_objs = repos.get_repo_objects()
-    for single_repo in repos.repo_list:
-        process = ProcessRepo(machine, ghinterface, actions, single_repo)
+    functions = get_approved_functions(rtdata)
+    repo_list = get_approved_repos(rtdata, machine, ghinterface)
+    for single_repo in repo_list:
+        for single_pr in single_repo.pullreq_list:
+            process_pr(single_pr, ghinterface, machine, functions)
+        # process = Process(machine, ghinterface, actions, single_repo)
 
 if __name__ == '__main__':
     main()
