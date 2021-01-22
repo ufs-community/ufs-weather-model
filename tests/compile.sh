3d6236495e76d878462c6411cb08ae2616341cbb
William Ramstrom <William.Ramstrom@noaa.gov>
2021-01-22 10:36:37 -0600

    Initial Moving Nest Functionality

--- tests/compile.sh
+++ tests/compile.sh
@@ -94,6 +94,12 @@ if [[ "${MAKE_OPT}" == *"OPENMP=N"* ]]; then
   CMAKE_FLAGS="${CMAKE_FLAGS} -DOPENMP=OFF"
 fi
 
+if [[ "${MAKE_OPT}" == *"MOVING_NEST=Y"* ]]; then
+  CMAKE_FLAGS="${CMAKE_FLAGS} -DMOVING_NEST=ON"
+else
+  CMAKE_FLAGS="${CMAKE_FLAGS} -DMOVING_NEST=OFF"
+fi
+
 if [[ "${MAKE_OPT}" == *"MULTI_GASES=Y"* ]]; then
     CMAKE_FLAGS="${CMAKE_FLAGS} -DMULTI_GASES=ON"
 else
