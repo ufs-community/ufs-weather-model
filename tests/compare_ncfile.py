e68a134ae8cdff57ab6f43210778abaaeb3f1873
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2021-01-26 14:25:39 -0600

    Make compare_ncfile.py return 2 if files are different since it returns 1 if error occurs.

--- tests/compare_ncfile.py
+++ tests/compare_ncfile.py
@@ -6,10 +6,10 @@ from netCDF4 import Dataset
 with Dataset(sys.argv[1]) as nc1, Dataset(sys.argv[2]) as nc2:
   if nc1.variables.keys()!=nc2.variables.keys():
     print("Variables are different")
-    sys.exit(1)
+    sys.exit(2)
 
   for varname in nc1.variables.keys():
     diff = nc2[varname][:]-nc1[varname][:]
     if (np.abs(diff)).max() != 0:
       print(varname,"is different")
-      sys.exit(1)
+      sys.exit(2)
