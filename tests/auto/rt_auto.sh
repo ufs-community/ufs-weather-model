25ff215414746b35ba1dea150b6453c90c1f9ca9
Brian Curtis <brian.curtis@noaa.gov>
2021-03-01 19:13:58 +0000

    Add permission check for 600 in accesstoken.sh

--- tests/auto/rt_auto.sh
+++ tests/auto/rt_auto.sh
@@ -1,7 +1,12 @@
 #!/bin/bash --login
 set -eux
 if [ -f "accesstoken.sh" ]; then
-  source ./accesstoken.sh
+  if [ $(stat -c "%a" "accesstoken.sh") == "600" ]; then
+    echo "Sourcing accesstoken.sh"
+    source ./accesstoken.sh
+  else
+    echo "accesstoken.sh permissions NEED to be set to 600 before starting"
+  fi
 else
   echo "Please create accesstoken.sh (600) with the following content\n"
   echo "export ghapitoken=<GitHub API Token Here>"
@@ -31,14 +36,10 @@ elif [[ $HOSTNAME == gaea* ]]; then
   export ACCNR="nggps_emc" # This applies to Brian.Curtis, may need change later
   export PATH=/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/envs/ufs-weather-model/bin:$PATH
   export PYTHONPATH=/lustre/f2/pdata/esrl/gsd/contrib/miniconda3/4.8.3/lib/python3.8/site-packages
-elif [[ $HOSTNAME = cheyenne*.cheyenne.ucar.edu ]]; then
+elif [[ $HOSTNAME = cheyenne* ]]; then
   MACHINE_NAME=cheyenne
   WORKDIR=/glade/work/heinzell/fv3/ufs-weather-model/auto-rt
   export ACCNR="P48503002"
-  # DH* 20210226 temporary workaround to be able to use gnu
-  # export RT_COMPILER="gnu"
-  # export MACHINE_ID="cheyenne.gnu"
-  # *DH 20210226
   export PATH=/glade/p/ral/jntp/tools/miniconda3/4.8.3/envs/ufs-weather-model/bin:/glade/p/ral/jntp/tools/miniconda3/4.8.3/bin:$PATH
   export PYTHONPATH=/glade/p/ral/jntp/tools/miniconda3/4.8.3/envs/ufs-weather-model/lib/python3.8/site-packages:/glade/p/ral/jntp/tools/miniconda3/4.8.3/lib/python3.8/site-packages
 else
