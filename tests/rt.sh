f6a7b0ac92b5969eb25692cb1d96ee5a61f34f71
Jun Wang <junwang-noaa@users.noreply.github.com>
2021-03-10 03:45:12 +0000

    update post_itag_fv3lam

--- tests/rt.sh
+++ tests/rt.sh
@@ -415,9 +415,9 @@ if [[ $TESTS_FILE =~ '35d' ]]; then
 fi
 
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = gaea.* ]] || [[ $MACHINE_ID = jet.* ]]; then
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20210308/${RT_COMPILER^^}}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20210309/${RT_COMPILER^^}}
 else
-  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20210308}
+  RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-20210309}
 fi
 
 INPUTDATA_ROOT=${INPUTDATA_ROOT:-$DISKNM/NEMSfv3gfs/input-data-20210212}
