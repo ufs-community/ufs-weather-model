d7ba640106a9a8c54a1d03b1db7d358b56d33f6d
Dusan Jovic <dusan.jovic@noaa.gov>
2021-05-03 19:03:06 +0000

    Remove NETCDF and -DNETCDF_DIR from build.sh

--- build.sh
+++ build.sh
@@ -11,14 +11,12 @@ export CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-${CC:-mpicc}}
 export CMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER:-${CXX:-mpicxx}}
 export CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-${FC:-mpif90}}
 
-export NETCDF=${NETCDF:-${NETCDF_ROOT:?"Please set NETCDF/NETCDF_ROOT environment variable"}}
 export ESMFMKFILE=${ESMFMKFILE:?"Please set ESMFMKFILE environment variable"}
 
 BUILD_DIR=${BUILD_DIR:-${UFS_MODEL_DIR}/build}
 mkdir -p ${BUILD_DIR}
 
 [[ -n "${CCPP_SUITES:-""}" ]] && CMAKE_FLAGS+=" -DCCPP_SUITES=${CCPP_SUITES}"
-CMAKE_FLAGS+=" -DNETCDF_DIR=${NETCDF}"
 
 cd ${BUILD_DIR}
 cmake ${UFS_MODEL_DIR} ${CMAKE_FLAGS}
