51c02e407b4953cb283686ed39fc3c91f516076b
Ufuk Turuncoglu <ufuk.turuncoglu@noaa.gov>
2021-05-25 00:41:25 -0500

    fix merge conflict issue

--- tests/compile.sh
+++ tests/compile.sh
@@ -83,11 +83,7 @@ echo "Compiling ${MAKE_OPT} into $BUILD_NAME.exe on $MACHINE_ID"
 CMAKE_FLAGS=''
 
 if [[ "${MAKE_OPT}" == *"DEBUG=Y"* ]]; then
-<<<<<<< HEAD
-  CMAKE_FLAGS="${CMAKE_FLAGS} -DDEBUG=ON"
-=======
   CMAKE_FLAGS="${CMAKE_FLAGS} -DDEBUG=ON -DCMAKE_BUILD_TYPE=Debug"
->>>>>>> 7d18241
 elif [[ "${MAKE_OPT}" == *"REPRO=Y"* ]]; then
   CMAKE_FLAGS="${CMAKE_FLAGS} -DREPRO=ON"
 fi
