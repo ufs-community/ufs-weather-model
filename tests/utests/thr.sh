bb4e1c80fc0d1148d0e7c90b8a1623172348a7ef
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2021-06-02 11:38:53 -0500

    Add docker rmi. Fix thr configs for cpld

--- tests/utests/thr.sh
+++ tests/utests/thr.sh
@@ -11,10 +11,17 @@ elif [[ $application == 'regional' ]]; then
   TPN=$((TPN/THRD))
   NODES=$(((TASKS+TPN-1)/TPN))
 elif [[ $application == 'cpld' ]]; then
-  JNPES=$((JNPES/THRD))
-  TASKS=$((INPES*JNPES*6 + WRTIE_GROUP*WRTTASK_PER_GROUP + 10 + 6))
-  TPN=$((TPN/THRD))
-  NODES=$(((TASKS+TPN-1)/TPN))
+  if [[ $CI_TEST != 'true' ]]; then
+    INPES=3
+    JNPES=4
+    med_petlist_bounds="0 71"
+    atm_petlist_bounds="0 77"
+    ocn_petlist_bounds="78 107"
+    ice_petlist_bounds="108 119"
+    TASKS=$((INPES*JNPES*6 + WRTIE_GROUP*WRTTASK_PER_GROUP + 30 + 12))
+    TPN=$((TPN/THRD))
+    NODES=$(((TASKS+TPN-1)/TPN))
+  fi
 fi
 
 (test $CI_TEST == 'true') && source $PATHRT/utests/cmp_proc_bind.sh
