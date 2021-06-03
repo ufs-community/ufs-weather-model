2c882f0efe1c92914f4bd6f7a5f2cb586404bde9
Minsuk Ji <57227195+MinsukJi-NOAA@users.noreply.github.com>
2021-06-03 12:40:28 -0400

    Update utest and ci (#603)
    * Update utest script: Handle global, regional, and coupled tests
    * Update CI workflow: Instead of saving the docker image (~730 MB) as an artifact, only save the executable (~77 MB)
    * Add regional thread and decomposition tests to CI
    * Add coupled restart test to CI
    * Fix to run threads on its own core in ec2 for thread tests
    * Use a slightly different ubuntu-hpc image to match the uid and gid within ec2 instances
    * Exclude L127 control test temporarily

---
+++ tests/utests/std.sh
@@ -0,0 +1,33 @@
+set -eu
+
+if [[ $application == 'global' ]]; then
+  if [[ $CI_TEST == 'true' ]]; then
+    INPES=3
+    JNPES=2
+    TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP))
+    RESTART_INTERVAL=$((FHMAX/2))
+  fi
+elif [[ $application == 'regional' ]]; then
+  if [[ $CI_TEST == 'true' ]]; then
+    INPES=4
+    JNPES=6
+    TASKS=$((INPES*JNPES))
+  fi
+elif [[ $application == 'cpld' ]]; then
+  if [ $CI_TEST == 'true' ]; then
+    INPES=2
+    JNPES=2
+    NPROC_ICE=6
+    med_petlist_bounds="0 23"
+    atm_petlist_bounds="0 29"
+    ocn_petlist_bounds="30 39"
+    ice_petlist_bounds="40 45"
+    TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP + 10 + 6))
+  fi
+fi
+
+NODES=$(((TASKS+TPN-1)/TPN))
+(test $CI_TEST == 'true') && source $PATHRT/utests/cmp_proc_bind.sh
+if [[ $RT_SUFFIX =~ std ]]; then
+  source $PATHRT/utests/wrt_env.sh
+fi
