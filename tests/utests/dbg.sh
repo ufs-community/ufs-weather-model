265d707d40f5472e951b564827bd5e9005784aa6
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2021-06-17 08:54:41 -0400

    Fix HF output for control dbg. Add cpld dbg

--- tests/utests/dbg.sh
+++ tests/utests/dbg.sh
@@ -4,13 +4,16 @@ source $PATHRT/utests/std.sh
 if [[ $application == 'global' ]]; then
   LIST_FILES="sfcf000.nc sfcf001.nc atmf000.nc atmf001.nc"
   FHMAX=1
+  NFHOUT=3
+  NFHMAX_HF=12
+  NFHOUT_HF=1
   WLCLK=60
 elif [[ $application == 'regional' ]]; then
   echo "Regional application not yet implemented for debug"
   exit 1
 elif [[ $application == 'cpld' ]]; then
-  echo "Coupled application not yet implemented for debug"
-  exit 1
+  FHMAX=6
+  NFHOUT_HF=1
 fi
 
 source $PATHRT/utests/wrt_env.sh
