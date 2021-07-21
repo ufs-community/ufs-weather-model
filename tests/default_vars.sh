e6ca294568aadacd85d55067977486cd846b19ef
Samuel Trahan (NOAA contractor) <39415369+SamuelTrahanNOAA@users.noreply.github.com>
2021-07-21 11:39:23 -0400

    Feature/gsl develop diagnostics 20210609 (#660)
    * Change the timestep for the fv3_gsd and fv3_gsd_debug tests. Parallels use this new timestep to avoid crashes
    * Per-timestep surface pressure diagnostics, similar to WRF
    * Diagnostic 3D tendencies for all tracers
    * There are two new tests: control_diag_debug and fv3_gsd_debug

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -574,6 +574,7 @@ export FHCYC=24
 export FHROT=0
 export LDIAG3D=.F.
 export QDIAG3D=.F.
+export PRINT_DIFF_PGR=.false.
 export MAX_OUTPUT_FIELDS=300
 
 # Stochastic physics
@@ -615,6 +616,10 @@ export ww3gline="'glo_30m'  'no' 'no' 'CPL:native' 'no' 'no' 'no' 'no' 'no' 'no'
 # Regional
 export WRITE_RESTART_WITH_BCS=.false.
 
+# Diagnostics
+export PRINT_DIFF_PGR=.false.
+
+# Coupling
 export coupling_interval_fast_sec=0
 }
 
@@ -627,6 +632,8 @@ export DATM_CDEPS=false
 
 export DAYS="1"
 export FHMAX="24"
+export FDIAG="6"
+export FHZERO="6"
 export WLCLK=30
 
 # default atm/ocn/ice resolution
@@ -683,6 +690,8 @@ export CCPP_SUITE="FV3_GFS_2017_coupled"
 export INPUT_NML=input.mom6_ccpp.nml.IN
 export FIELD_TABLE="field_table"
 export DIAG_TABLE="diag_table_template"
+
+export DIAG_TABLE_ADDITIONAL=''
 
 export FHROT='0'
 export NSOUT='-1'
