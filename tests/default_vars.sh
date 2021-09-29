9ba6cf53f6fe4b8edc9db6179eac0069363afe51
pjpegion <Philip.Pegion@noaa.gov>
2021-09-29 18:11:39 +0000

    fix typo in default_vars.sh

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -601,7 +601,7 @@ export CDMBWD_c384='1.1,0.72,1.0,1.0'
 export CDMBWD_c768='4.0,0.15,1.0,1.0'
 
 # CA tunable parameter
-export RCELL_c46=0.1 
+export RCELL_c48=0.1 
 export RCELL_c96=0.1 
 export RCELL_c192=0.2
 export RCELL_c384=0.72
