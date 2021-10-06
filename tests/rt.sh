b9d6244cb0287e893c1a38a5cda077e81b4c2786
Dustin Swales <dustin.swales@noaa.gov>
2021-10-06 17:46:01 +0000

    Updated baseline date

--- tests/rt.sh
+++ tests/rt.sh
@@ -242,13 +242,14 @@ elif [[ $MACHINE_ID = hera.* ]]; then
   module load ecflow
   ECFLOW_START=ecflow_start.sh
 
-  QUEUE=batch
-  COMPILE_QUEUE=batch
+  QUEUE=debug
+  COMPILE_QUEUE=debug
 
-  #ACCNR=fv3-cpu
+  ACCNR=ome
   PARTITION=
   dprefix=/scratch1/NCEPDEV
   DISKNM=$dprefix/nems/emc.nemspara/RT
+  dprefix=/scratch2/BMC/ome/Dustin.Swales/UFS
   STMP=$dprefix/stmp4
   PTMP=$dprefix/stmp2
 
@@ -444,7 +445,7 @@ if [[ $TESTS_FILE =~ '35d' ]]; then
   TEST_35D=true
 fi
 
-BL_DATE=20211004
+BL_DATE=20211006
 if [[ $MACHINE_ID = hera.* ]] || [[ $MACHINE_ID = orion.* ]] || [[ $MACHINE_ID = cheyenne.* ]] || [[ $MACHINE_ID = gaea.* ]] || [[ $MACHINE_ID = jet.* ]] || [[ $MACHINE_ID = s4.* ]]; then
   RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-${BL_DATE}/${RT_COMPILER^^}}
 else
