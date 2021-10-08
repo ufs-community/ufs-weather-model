85bb52976c45d62089bad86354e5d9eb51e78b9f
uturuncoglu <turuncu@ucar.edu>
2021-10-08 10:47:52 -0600

    update NoahMP

--- NOAHMP-interface/noahmp_files.cmake
+++
@@ -1,8 +0,0 @@
-list(APPEND _noahmp_src_files NOAHMP/src/module_sf_noahmplsm.F
-                              NOAHMP/src/lnd_comp_kind.F90
-                              NOAHMP/src/lnd_comp_types.F90
-                              NOAHMP/src/lnd_comp_shr.F90
-                              NOAHMP/src/lnd_import_export.F90
-                              NOAHMP/src/lnd_comp_nuopc.F90
-                              NOAHMP/src/lnd_comp_driver.F90
-                              externals/module_sf_gecros.F)
