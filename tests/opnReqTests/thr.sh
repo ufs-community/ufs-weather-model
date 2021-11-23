f8aaa6dc5e85dd04683d3cc262330cef6feac2bb
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2021-11-23 02:25:58 +0000

    ORT tests for cpld_control_p7 and cpld_bmark_p7

--- tests/opnReqTests/thr.sh
+++ tests/opnReqTests/thr.sh
@@ -2,25 +2,40 @@ set -eu
 source $PATHRT/opnReqTests/std.sh
 
 THRD=2
+TPN=$((TPN/THRD))
 if [[ $application == 'global' ]]; then
   JNPES=$((JNPES/THRD))
   TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP))
-  TPN=$((TPN/THRD))
   NODES=$(((TASKS+TPN-1)/TPN))
 elif [[ $application == 'regional' ]]; then
-  TPN=$((TPN/THRD))
   NODES=$(((TASKS+TPN-1)/TPN))
 elif [[ $application == 'cpld' ]]; then
   if [[ $CI_TEST != 'true' ]]; then
-    INPES=3
-    JNPES=4
-    med_petlist_bounds="0 71"
-    atm_petlist_bounds="0 77"
-    ocn_petlist_bounds="78 107"
-    ice_petlist_bounds="108 119"
-    TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP + 30 + 12))
-    TPN=$((TPN/THRD))
-    NODES=$(((TASKS+TPN-1)/TPN))
+    if [[ $TEST_NAME == 'cpld_control_c96_p7' ]]; then
+      INPES=3
+      JNPES=4
+      med_petlist_bounds="0 71"
+      atm_petlist_bounds="0 77"
+      ocn_petlist_bounds="78 107"
+      ice_petlist_bounds="108 119"
+      TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP + 30 + 12))
+      NODES=$(((TASKS+TPN-1)/TPN))
+    elif [[ $TEST_NAME == 'cpld_control_p7' ]]; then
+      INPES=3
+      JNPES=4
+      med_petlist_bounds="0 71"
+      atm_petlist_bounds="0 77"
+      ocn_petlist_bounds="78 97"
+      ice_petlist_bounds="98 107"
+      wav_petlist_bounds="108 119"
+      TASKS=$((INPES*JNPES*6 + WRITE_GROUP*WRTTASK_PER_GROUP + 20 + 10 + 12))
+      NODES=$(((TASKS+TPN-1)/TPN))
+    elif [[ $TEST_NAME == 'cpld_bmark_p7' ]]; then
+      NODES=$(((TASKS+TPN-1)/TPN))
+    else
+      echo "This test is not yet set up for the thread test"
+      exit 1
+    fi
   fi
 fi
 
