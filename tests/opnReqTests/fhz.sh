634dc1d5cbe1e4e98c7d8837f4bfe6e98182921a
MinsukJi-NOAA <Minsuk.Ji@noaa.gov>
2021-12-24 13:19:24 +0000

    Put in safety check for fhzero test

--- tests/opnReqTests/fhz.sh
+++ tests/opnReqTests/fhz.sh
@@ -1,16 +1,18 @@
 set -eu
 source $PATHRT/opnReqTests/std.sh
 
-FHZERO=3
-
-FHMAX_2D=$(printf "%02d" $FHMAX)
-LIST_FILES=$(echo -n $LIST_FILES | sed -E "s/phyf024.nc ?//g" \
-                                 | sed -E "s/dynf024.nc ?//g" \
-                                 | sed -E "s/sfcf024.nc ?//g" \
-                                 | sed -E "s/atmf024.nc ?//g" \
-                                 | sed -E "s/GFSFLX.GrbF24 ?//g" \
-                                 | sed -E "s/GFSPRS.GrbF24 ?//g" \
-                                 | sed -e "s/^ *//" -e "s/ *$//")
+if [[ $application == 'global' ]]; then
+  FHZERO=3
+  LIST_FILES=$(echo -n $LIST_FILES | sed -E "s/phyf024.nc ?//g" \
+                                   | sed -E "s/dynf024.nc ?//g" \
+                                   | sed -E "s/sfcf024.nc ?//g" \
+                                   | sed -E "s/atmf024.nc ?//g" \
+                                   | sed -E "s/GFSFLX.GrbF24 ?//g" \
+                                   | sed -E "s/GFSPRS.GrbF24 ?//g" \
+                                   | sed -e "s/^ *//" -e "s/ *$//")
+else
+  echo "This test is not yet set up for the fhzero test"
+fi
 
 source $PATHRT/opnReqTests/wrt_env.sh
 
