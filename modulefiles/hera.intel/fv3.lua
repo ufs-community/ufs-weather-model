c21f107196aa80f3c13705a353ab4ff8da57d0c9
Jun Wang <junwang-noaa@users.noreply.github.com>
2022-02-08 18:24:37 +0000

    add fv3lua on hera and orion

---
+++ modulefiles/hera.intel/fv3.lua
@@ -0,0 +1,72 @@
+help([[
+Load environment to compile ufs-weather-model on Hera using Intel
+]])
+
+cmake_ver=os.getenv("cmake_ver") or "3.16.1"
+load(pathJoin("cmake", cmake_ver))
+
+prepend_path("MODULEPATH", " /contrib/anaconda/modulefiles")
+
+anaconda_ver=os.getenv("anaconda_ver") or "5.3.1"
+load(pathJoin("anaconda", anaconda_ver))
+
+prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+
+hpc_ver=os.getenv("hpc_ver") or "1.1.0"
+load(pathJoin("hpc", hpc_ver))
+
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
+load(pathJoin("hpc-intel", hpc_intel_ver))
+
+impi_ver=os.getenv("impi_ver") or "2018.0.4"
+load(pathJoin("hpc-impi", impi_ver))
+
+bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+load(pathJoin("bacio", bacio_ver))
+
+ip_ver=os.getenv("ip_ver") or "3.3.3"
+load(pathJoin("ip", ip_ver))
+
+sp_ver=os.getenv("sp_ver") or "2.3.3"
+load(pathJoin("sp", sp_ver))
+
+w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
+load(pathJoin("w3nco", w3nco_ver))
+
+w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
+load(pathJoin("w3emc", w3emc_ver))
+
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+load(pathJoin("nemsio", nemsio_ver))
+
+g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
+load(pathJoin("g2tmpl", g2tmpl_ver))
+
+crtm_ver=os.getenv("crtm_ver") or "2.3.0"
+load(pathJoin("crtm", crtm_ver))
+
+g2_ver=os.getenv("g2_ver") or "3.4.5"
+load(pathJoin("g2", g2_ver))
+
+upp_ver=os.getenv("upp_ver") or "8.1.0"
+load(pathJoin("upp", upp_ver))
+
+jasper_ver=os.getenv("jasper_ver") or "2.0.25"
+load(pathJoin("jasper", jasper_ver))
+
+zlib_ver=os.getenv("zlib_ver") or "1.2.11"
+load(pathJoin("zlib", zlib_ver))
+
+png_ver=os.getenv("png_ver") or "1.6.37"
+load(pathJoin("png", png_ver))
+
+hdf5_ver=os.getenv("hdf5_ver") or "1.10.6"
+load(pathJoin("hdf5", hdf5_ver))
+
+netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+load(pathJoin("netcdf", netcdf_ver))
+
+esmf_ver=os.getenv("esmf_ver") or "8_0_1"
+load(pathJoin("esmf", esmf_ver))
+
+whatis("Description: ufs-weather-model build environment")
