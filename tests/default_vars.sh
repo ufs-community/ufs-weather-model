244fee6bfeca3e01a907935efe8b2099fef0a386
jessica.meixner <Jessica.Meixner@noaa.gov>
2022-03-01 18:29:21 +0000

    fix typo for dell

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -68,7 +68,7 @@ elif [[ $MACHINE_ID = wcoss_dell_p3 ]]; then
 
   TASKS_cpl_bmrk_aero=560; TPN_cpl_bmrk_aero=14; INPES_cpl_bmrk_aero=6; JNPES_cpl_bmrk_aero=8
   THRD_cpl_bmrk_aero=2; WPG_cpl_bmrk_aero=24; MPB_cpl_bmrk_aero="0 287"; APB_cpl_bmrk_aero="0 311"
-  MPB_cpl_bmrk_aero="0 287"; OPB_cpl_bmrk_aero="312 431"; IPB_cpl_bmrk_aero="432 479"; WPB_cpl_bmrk_aero="480 559"
+  CHM_cpl_bmrk_aero="0 287"; OPB_cpl_bmrk_aero="312 431"; IPB_cpl_bmrk_aero="432 479"; WPB_cpl_bmrk_aero="480 559"
   NPROC_ICE_cpl_bmrk_aero=48
 
   TASKS_cpl_bmrk_mpi=600; TPN_cpl_bmrk_mpi=28; INPES_cpl_bmrk_mpi=6; JNPES_cpl_bmrk_mpi=8
