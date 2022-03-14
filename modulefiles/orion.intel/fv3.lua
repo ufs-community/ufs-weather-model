8f27993996c080ed2b9aa60ffec783b45e81ec37
Jun Wang <jun.wang@noaa.gov>
2022-03-14 08:49:27 -0500

    use intel 2018.4 for hpc-stack

--- modulefiles/orion.intel/fv3.lua
+++ modulefiles/orion.intel/fv3.lua
@@ -2,22 +2,22 @@ help([[
 Load environment to compile ufs-weather-model on Orion using Intel
 ]])
 
-cmake_ver=os.getenv("cmake_ver") or "3.20.2"
+cmake_ver=os.getenv("cmake_ver") or "3.22.1"
 load(pathJoin("cmake", cmake_ver))
 
 python_ver=os.getenv("python_ver") or "3.7.5"
 load(pathJoin("python", python_ver))
 
-prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack-gfsv16/modulefiles/stack")
 
-hpc_ver=os.getenv("hpc_ver") or "1.1.0"
+hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_intel_ver=os.getenv("hpc_intel_ver") or "2018.4"
+hpc_intel_ver=os.getenv("hpc-intel_ver") or "2018.4"
 load(pathJoin("hpc-intel", hpc_intel_ver))
 
-impi_ver=os.getenv("impi_ver") or "2018.4"
-load(pathJoin("hpc-impi", impi_ver))
+hpc_impi_ver=os.getenv("hpc-impi_ver") or "2018.4"
+load(pathJoin("hpc-impi", hpc_impi_ver))
 
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
 load(pathJoin("bacio", bacio_ver))
@@ -55,8 +55,8 @@ load(pathJoin("jasper", jasper_ver))
 zlib_ver=os.getenv("zlib_ver") or "1.2.11"
 load(pathJoin("zlib", zlib_ver))
 
-png_ver=os.getenv("png_ver") or "1.6.37"
-load(pathJoin("png", png_ver))
+libpng_ver=os.getenv("png_ver") or "1.6.37"
+load(pathJoin("libpng", libpng_ver))
 
 hdf5_ver=os.getenv("hdf5_ver") or "1.10.6"
 load(pathJoin("hdf5", hdf5_ver))
@@ -67,8 +67,8 @@ load(pathJoin("netcdf", netcdf_ver))
 esmf_ver=os.getenv("esmf_ver") or "8_0_1"
 load(pathJoin("esmf", esmf_ver))
 
-setenv("CMAKE_C_COMPILER",mpiicc)
-setenv("CMAKE_CXX_COMPILER",mpiicpc)
-setenv("CMAKE_Fortran_COMPILER",mpiifort)
-setenv("CMAKE_Platform",orion.intel)
+setenv("CMAKE_C_COMPILER","mpiicc")
+setenv("CMAKE_CXX_COMPILER","mpiicpc")
+setenv("CMAKE_Fortran_COMPILER","mpiifort")
+setenv("CMAKE_Platform","orion.intel")
 whatis("Description: ufs-weather-model build environment")
