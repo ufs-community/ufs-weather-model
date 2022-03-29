a537f2b39c9714c9b4c3b667ca0f4c7cb5bb2a9f
Jun Wang <37633869+junwang-noaa@users.noreply.github.com>
2022-03-29 16:18:35 -0400

    production/GFS.v16: update module files on hera/orion (#1040)
    * add fv3 lua and update fv3 module on hera and orion

---
+++ modulefiles/orion.intel/fv3.lua
@@ -0,0 +1,74 @@
+help([[
+Load environment to compile ufs-weather-model on Orion using Intel
+]])
+
+cmake_ver=os.getenv("cmake_ver") or "3.22.1"
+load(pathJoin("cmake", cmake_ver))
+
+python_ver=os.getenv("python_ver") or "3.7.5"
+load(pathJoin("python", python_ver))
+
+prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack-gfsv16/modulefiles/stack")
+
+hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+load(pathJoin("hpc", hpc_ver))
+
+hpc_intel_ver=os.getenv("hpc-intel_ver") or "2018.4"
+load(pathJoin("hpc-intel", hpc_intel_ver))
+
+hpc_impi_ver=os.getenv("hpc-impi_ver") or "2018.4"
+load(pathJoin("hpc-impi", hpc_impi_ver))
+
+bacio_ver=os.getenv("bacio_ver") or "2.4.1"
+load(pathJoin("bacio", bacio_ver))
+
+ip_ver=os.getenv("ip_ver") or "3.3.3"
+load(pathJoin("ip", ip_ver))
+
+sp_ver=os.getenv("sp_ver") or "2.3.3"
+load(pathJoin("sp", sp_ver))
+
+w3nco_ver=os.getenv("w3nco_ver") or "2.4.1"
+load(pathJoin("w3nco", w3nco_ver))
+
+w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
+load(pathJoin("w3emc", w3emc_ver))
+
+nemsio_ver=os.getenv("nemsio_ver") or "2.5.2"
+load(pathJoin("nemsio", nemsio_ver))
+
+g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
+load(pathJoin("g2tmpl", g2tmpl_ver))
+
+crtm_ver=os.getenv("crtm_ver") or "2.3.0"
+load(pathJoin("crtm", crtm_ver))
+
+g2_ver=os.getenv("g2_ver") or "3.4.5"
+load(pathJoin("g2", g2_ver))
+
+upp_ver=os.getenv("upp_ver") or "8.1.0"
+load(pathJoin("upp", upp_ver))
+
+jasper_ver=os.getenv("jasper_ver") or "2.0.25"
+load(pathJoin("jasper", jasper_ver))
+
+zlib_ver=os.getenv("zlib_ver") or "1.2.11"
+load(pathJoin("zlib", zlib_ver))
+
+libpng_ver=os.getenv("png_ver") or "1.6.37"
+load(pathJoin("libpng", libpng_ver))
+
+hdf5_ver=os.getenv("hdf5_ver") or "1.10.6"
+load(pathJoin("hdf5", hdf5_ver))
+
+netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
+load(pathJoin("netcdf", netcdf_ver))
+
+esmf_ver=os.getenv("esmf_ver") or "8_0_1"
+load(pathJoin("esmf", esmf_ver))
+
+setenv("CMAKE_C_COMPILER","mpiicc")
+setenv("CMAKE_CXX_COMPILER","mpiicpc")
+setenv("CMAKE_Fortran_COMPILER","mpiifort")
+setenv("CMAKE_Platform","orion.intel")
+whatis("Description: ufs-weather-model build environment")
