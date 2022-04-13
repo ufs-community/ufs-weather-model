6be35ed9203ca78f3c46fd83bd383511bd6ab359
Barry Baker <bbakernoaa@users.noreply.github.com>
2022-04-13 09:42:37 -0400

    add FSCAV_AERO to default coupled RT variables

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -1245,6 +1245,8 @@ export DO_SAT_ADJ=.false.
 export KTHERM=2
 export TFREEZE_OPTION=mushy
 
+export FSCAV_AERO='"*:0.3","so2:0.0","msa:0.0","dms:0.0","nh3:0.4","nh4:0.6","bc1:0.6","bc2:0.6","dust1:0.6","dust2:0.6","dust3:0.6","dust4:0.6","dust5:0.6","seas1:0.5","seas2:0.5","seas3:0.5","seas4:0.5","seas5:0.5"'
+
 export CPLFLX=.true.
 export CPLICE=.true.
 export CPL=.true.
