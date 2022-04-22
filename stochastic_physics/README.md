50b3fea5ba941b014c5ab9decbef41cd952ab08b
Lisa Bengtsson <Lisa.Bengtsson@noaa.gov>
2022-04-22 20:18:12 +0000

    update stochastic_physics submodule

--- stochastic_physics/README.md
+++
@@ -1,3 +0,0 @@
-# stochastic_physics
-
-Contains the stochastic physics pattern generator for the Unified Forecast System developed at NOAA/ESRL/PSL.
--- stochastic_physics/unit_tests/compile_compare.sh
+++
@@ -1,1 +0,0 @@
-mpif90 -C -traceback -o compare_output -I${NETCDF}/include compare_output.F90 -L${NETCDF}/lib -lnetcdff -lnetcdf -L${HDF5_LIBRARIES} -lhdf5_hl -lhdf5 -L${ZLIB_LIBRARIES} -lz
