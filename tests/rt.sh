4d629751e8fd7634233a61d1cad53bb1c7734495
denise.worthen <Denise.Worthen@noaa.gov>
2022-07-22 12:58:20 +0000

    point to default input-data directory

--- tests/rt.sh
+++ tests/rt.sh
@@ -424,9 +424,7 @@ BL_DATE=20220720
 
 RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-${BL_DATE}/${RT_COMPILER^^}}
 
-#INPUTDATA_ROOT=${INPUTDATA_ROOT:-$DISKNM/NEMSfv3gfs/input-data-20220414}
-#INPUTDATA_ROOT="/scratch1/NCEPDEV/stmp4/Denise.Worthen/input-data-20220414"
-INPUTDATA_ROOT="/work/noaa/stmp/dworthen/stmp/dworthen/input-data-20220414"
+INPUTDATA_ROOT=${INPUTDATA_ROOT:-$DISKNM/NEMSfv3gfs/input-data-20220414}
 INPUTDATA_ROOT_WW3=${INPUTDATA_ROOT}/WW3_input_data_20220624
 INPUTDATA_ROOT_BMIC=${INPUTDATA_ROOT_BMIC:-$DISKNM/NEMSfv3gfs/BM_IC-20220207}
 
@@ -522,7 +520,7 @@ if [[ $ECFLOW == true ]]; then
   MAX_JOBS=30
 
   # Default number of tries to run jobs - on wcoss, no error tolerance
-  ECF_TRIES=1
+  ECF_TRIES=2
   if [[ $MACHINE_ID = wcoss* ]]; then
     ECF_TRIES=1
   fi
