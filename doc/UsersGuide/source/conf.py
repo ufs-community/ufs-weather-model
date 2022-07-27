9b13cbdf059446b76ed7455287045935b4b3db97
gspetro <gillian.s.petro@gmail.com>
2022-07-27 14:02:09 -0400

    adjust expand theme width

--- doc/UsersGuide/source/conf.py
+++ doc/UsersGuide/source/conf.py
@@ -104,6 +104,7 @@ html_theme_options = {"body_max_width": "none"}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_context = {}
 
 def setup(app):
     app.add_css_file('custom.css')  # may also be an URL
