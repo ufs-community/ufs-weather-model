1649f4a5c8a34034eb993f2be34bddcce1e26861
gspetro <gillian.s.petro@gmail.com>
2022-07-27 12:10:58 -0400

    change sphinx theme & pygments_style

--- doc/UsersGuide/source/conf.py
+++ doc/UsersGuide/source/conf.py
@@ -81,7 +81,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -89,7 +89,9 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+#html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
+#html_theme_path = ["_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
