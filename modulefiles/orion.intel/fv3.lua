597d97503dee508151a806f0de99965f2fafed22
Jun Wang <junwang-noaa@users.noreply.github.com>
2022-09-19 18:55:46 +0000

    update crtm and upp lib

--- modulefiles/orion.intel/fv3.lua
+++ modulefiles/orion.intel/fv3.lua
@@ -8,7 +8,7 @@ load(pathJoin("cmake", cmake_ver))
 python_ver=os.getenv("python_ver") or "3.7.5"
 load(pathJoin("python", python_ver))
 
-prepend_path("MODULEPATH", "/apps/contrib/NCEP/libs/hpc-stack-gfsv16/modulefiles/stack")
+prepend_path("MODULEPATH", "/apps/contrib/NCEP/hpc-stack/libs/hpc-stack-gfsv16/modulefiles/stack")
 
 hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
@@ -40,13 +40,13 @@ load(pathJoin("nemsio", nemsio_ver))
 g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
 load(pathJoin("g2tmpl", g2tmpl_ver))
 
-crtm_ver=os.getenv("crtm_ver") or "2.3.0"
+crtm_ver=os.getenv("crtm_ver") or "2.4.0"
 load(pathJoin("crtm", crtm_ver))
 
 g2_ver=os.getenv("g2_ver") or "3.4.5"
 load(pathJoin("g2", g2_ver))
 
-upp_ver=os.getenv("upp_ver") or "8.1.0"
+upp_ver=os.getenv("upp_ver") or "8.2.0"
 load(pathJoin("upp", upp_ver))
 
 jasper_ver=os.getenv("jasper_ver") or "2.0.25"
