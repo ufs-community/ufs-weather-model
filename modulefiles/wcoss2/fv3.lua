cf108c8eef262894d8a0b26611ecdcdd11ab233a
Jun Wang <37633869+junwang-noaa@users.noreply.github.com>
2022-10-18 08:55:22 -0400

    production/GFSv16: Remove nowarn compile option and update crtm/2.4.0 on wcoss2  (#1459)
    * remove nowarn, update crtm on wcoss2 and clean up

--- modulefiles/wcoss2/fv3.lua
+++ modulefiles/wcoss2/fv3.lua
@@ -38,15 +38,14 @@ load(pathJoin("nemsio", nemsio_ver))
 g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.9.1"
 load(pathJoin("g2tmpl", g2tmpl_ver))
 
--- prod upp library is not available, need to update later
-crtm_ver=os.getenv("crtm_ver") or "2.3.0"
+upp_ver=os.getenv("upp_ver") or "8.2.0"
+load(pathJoin("upp", upp_ver))
+
+crtm_ver=os.getenv("crtm_ver") or "2.4.0"
 load(pathJoin("crtm", crtm_ver))
 
 g2_ver=os.getenv("g2_ver") or "3.4.5"
 load(pathJoin("g2", g2_ver))
-
-upp_ver=os.getenv("upp_ver") or "8.2.0"
-load(pathJoin("upp", upp_ver))
 
 jasper_ver=os.getenv("jasper_ver") or "2.0.25"
 load(pathJoin("jasper", jasper_ver))
