a57a1dda58b7542f0e5e5bfc50c8eb868d64b547
ChunxiZhang-NOAA <49283036+ChunxiZhang-NOAA@users.noreply.github.com>
2022-11-08 11:41:19 -0500

    Improve radiative fluxes and cloud cover in FV3 for HR1 (#1481)
    * Improve radiative fluxes and cloud cover in FV3 for HR1
    
    Co-authored-by: JONG KIM <jong.kim@noaa.gov>
    Co-authored-by: Brian Curtis <brian.curtis@noaa.gov>

--- tests/rt.sh
+++ tests/rt.sh
@@ -443,7 +443,7 @@ if [[ $TESTS_FILE =~ '35d' ]] || [[ $TESTS_FILE =~ 'weekly' ]]; then
   TEST_35D=true
 fi
 
-BL_DATE=20221101
+BL_DATE=20221107
 
 RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-${BL_DATE}/${RT_COMPILER^^}}
 
