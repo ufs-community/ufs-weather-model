fa7e70ef656197d89d02abf3a5e2ee20d64502ae
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2022-11-08 15:49:17 -0500

    Update ufs_hera.gnu.lua
    Use updated location for the hpc-stack with gnu/9.2.0 and mpich/3.3.2, in EPIC-managed space

--- modulefiles/ufs_hera.gnu.lua
+++ modulefiles/ufs_hera.gnu.lua
@@ -12,12 +12,15 @@ load("sutils")
 cmake_ver=os.getenv("cmake_ver") or "3.20.1"
 load(pathJoin("cmake", cmake_ver))
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+gnu_ver=os.getenv("gnu_ver") or "9.2.0"
+load(pathJoin("gnu", gnu_ver))
 
-hpc_ver=os.getenv("hpc_ver") or "1.1.0"
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/gnu-9.2/modulefiles/stack")
+
+hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "9.2.0"
+hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "9.2"
 load(pathJoin("hpc-gnu", hpc_gnu_ver))
 
 hpc_mpich_ver=os.getenv("hpc_mpich_ver") or "3.3.2"
