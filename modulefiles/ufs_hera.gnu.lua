ab07bb52cb8ca48dd0ee60e127df985dddf97571
Natalie Perlin <68030316+natalie-perlin@users.noreply.github.com>
2022-11-09 21:48:57 -0500

    Update ufs_hera.gnu.lua
    Modified gnu/9.2- mpi combination to use openmpi/3.1.4, as  requested (Attn: @jkbk2004 )

--- modulefiles/ufs_hera.gnu.lua
+++ modulefiles/ufs_hera.gnu.lua
@@ -15,16 +15,16 @@ load(pathJoin("cmake", cmake_ver))
 gnu_ver=os.getenv("gnu_ver") or "9.2.0"
 load(pathJoin("gnu", gnu_ver))
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/gnu-9.2/modulefiles/stack")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/gnu-9.2_openmpi-3.1.4/modulefiles/stack")
 
 hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "9.2"
+hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "9.2.0"
 load(pathJoin("hpc-gnu", hpc_gnu_ver))
 
-hpc_mpich_ver=os.getenv("hpc_mpich_ver") or "3.3.2"
-load(pathJoin("hpc-mpich", hpc_mpich_ver))
+hpc_mpich_ver=os.getenv("hpc_openmpi_ver") or "3.1.4"
+load(pathJoin("hpc-openmpi", hpc_mpich_ver))
 
 load("ufs_common")
 
