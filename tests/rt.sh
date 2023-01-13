fba42f1fccb34bdde98093d68b98deac61b5143f
Eric Aligo <eric.aligo@noaa.gov>
2023-01-13 14:45:27 +0000

    commit correct hash of upp and atmos_cubed_sphere to winterwx

--- tests/rt.sh
+++ tests/rt.sh
@@ -217,7 +217,7 @@ elif [[ $MACHINE_ID = hera.* ]]; then
   QUEUE=batch
   COMPILE_QUEUE=batch
 
-  #ACCNR="${ACCNR:-fv3-cpu}
+  ACCNR="${ACCNR:-fv3-cam}"
   PARTITION=
   dprefix=/scratch1/NCEPDEV
   DISKNM=$dprefix/nems/emc.nemspara/RT
