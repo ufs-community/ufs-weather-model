30abda4959a588adf22e045284a4bd4da55a2fa9
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-01-31 14:23:41 -0500

    Update ufs_hera.gnu.lua

--- modulefiles/ufs_hera.gnu.lua
+++ modulefiles/ufs_hera.gnu.lua
@@ -12,15 +12,17 @@ load("sutils")
 cmake_ver=os.getenv("cmake_ver") or "3.20.1"
 load(pathJoin("cmake", cmake_ver))
 
-gnu_ver=os.getenv("gnu_ver") or "9.2.0"
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/gnu/modulefiles")
+
+gnu_ver=os.getenv("gnu_ver") or "10.2.0"
 load(pathJoin("gnu", gnu_ver))
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/gnu-9.2/modulefiles/stack")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/gnu-10.2/modulefiles/stack")
 
 hpc_ver=os.getenv("hpc_ver") or "1.2.0"
 load(pathJoin("hpc", hpc_ver))
 
-hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "9.2"
+hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "10.2.0"
 load(pathJoin("hpc-gnu", hpc_gnu_ver))
 
 hpc_mpich_ver=os.getenv("hpc_mpich_ver") or "3.3.2"
