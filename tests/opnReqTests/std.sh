7a17b868b1012f877e00be29ca5f0779a94caf4c
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-01-31 16:29:58 -0500

    Update std.sh

--- tests/opnReqTests/std.sh
+++ tests/opnReqTests/std.sh
@@ -6,7 +6,7 @@ if [[ $application == 'global' ]]; then
     JNPES=2
     WRITE_GROUP=1
     WRTTASK_PER_GROUP=12
-    TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP))
+    #TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP))
   fi
   RESTART_N=$(( FHMAX/2 ))
   RESTART_INTERVAL="${RESTART_N} -1"
@@ -16,7 +16,7 @@ elif [[ $application == 'regional' ]]; then
     JNPES=6
     NTILES=1
     WRTTASK_PER_GROUP=8
-    TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP))
+    #TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP))
   fi
 elif [[ $application == 'cpld' ]]; then
   if [ $CI_TEST == 'true' ]; then
@@ -25,13 +25,13 @@ elif [[ $application == 'cpld' ]]; then
     OCN_tasks=10
     ICE_tasks=6
     NPROC_ICE=$ICE_tasks
-    TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP + OCN_tasks + ICE_tasks))
+    #TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP + OCN_tasks + ICE_tasks))
   fi
   RESTART_N=$(( FHMAX/2 ))
   RESTART_INTERVAL="${RESTART_N} -1"
 fi
 
-NODES=$(((TASKS+TPN-1)/TPN))
+#NODES=$(((TASKS+TPN-1)/TPN))
 (test $CI_TEST == 'true') && source $PATHRT/opnReqTests/cmp_proc_bind.sh
 if [[ $RT_SUFFIX =~ std ]]; then
   source $PATHRT/opnReqTests/wrt_env.sh
