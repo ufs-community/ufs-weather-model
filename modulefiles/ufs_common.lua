0b483c1c79e254bb1845419f613a9da5f905602f
uturuncoglu <turuncu@ucar.edu>
2023-02-07 15:44:39 -0700

    update esmf and remove fms dependency

--- modulefiles/ufs_common.lua
+++ modulefiles/ufs_common.lua
@@ -17,10 +17,10 @@ load(pathJoin("hdf5", hdf5_ver))
 netcdf_ver=os.getenv("netcdf_ver") or "4.7.4"
 load(pathJoin("netcdf", netcdf_ver))
 
-pio_ver=os.getenv("pio_ver") or "2.5.7"
+pio_ver=os.getenv("pio_ver") or "2.5.8"
 load(pathJoin("pio", pio_ver))
 
-esmf_ver=os.getenv("esmf_ver") or "8.3.0b09"
+esmf_ver=os.getenv("esmf_ver") or "8.5.0b10"
 load(pathJoin("esmf", esmf_ver))
 
 fms_ver=os.getenv("fms_ver") or "2022.04"
