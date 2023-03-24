22de3614e430bf8835f7b3aabecb146a89bd0f84
JONG KIM <jong.kim@noaa.gov>
2023-03-24 15:31:05 -0400

    add new BL_DATE

--- tests/rt.sh
+++ tests/rt.sh
@@ -446,7 +446,7 @@ if [[ $TESTS_FILE =~ '35d' ]] || [[ $TESTS_FILE =~ 'weekly' ]]; then
 fi
 
 
-BL_DATE=20230321
+BL_DATE=20230324
 
 RTPWD=${RTPWD:-$DISKNM/NEMSfv3gfs/develop-${BL_DATE}/${RT_COMPILER^^}}
 
