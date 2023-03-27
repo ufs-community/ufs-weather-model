85fd336e829e0653ae729a96955b926d0d368028
zach1221 <99902696+zach1221@users.noreply.github.com>
2023-03-27 10:03:25 -0400

    Update ufs_noaacloud.intel_debug.lua

--- modulefiles/ufs_noaacloud.intel_debug.lua
+++ modulefiles/ufs_noaacloud.intel_debug.lua
@@ -1,19 +1,18 @@
-#%Module
-module purge
-module unuse /opt/cray/craype/default/modulefiles
-module unuse /opt/cray/modulefiles
-module use /contrib/spack-stack/miniconda/modulefiles/miniconda
-module use /contrib/spack-stack/envs/ufs-wm/install/modulefiles/Core
-module load stack-intel/2021.3.0
-module load stack-intel/2021.3.0-oneapi-mpi/2021.3.0
-module load stack-python/3.9.12
-module load cmake/3.23.1
+help([[loads UFS Model prerequisites for cloud/intel]])
 
-module load jasper/2.0.32 zlib/1.2.13 libpng/1.6.37 hdf5/1.10.6 netcdf-c/4.9.0 netcdf-fortran/4.6.0 parallelio/2.5.9 esmf/8.3.0b09-debug fms/2022.04 bacio/2.4.1 
-module load crtm-fix/2.4.0_emc g2/3.4.5 g2tmpl/1.10.2 ip/3.3.3 sp/2.3.3 w3nco/2.4.1 gftl-shared/1.5.0 yafyaml/0.5.1 mapl/2.22.0-esmf-8.3.0b09-debug w3emc/2.9.2
-module load crtm/2.4.0
+prepend_path("MODULE_PATH", "/contrib/spack-stack/envs/ufs-wm/install/modulefiles/Core")
 
-export CC=mpiicc
-export CXX=mpiicpc
-export FC=mpiifort
-export CMAKE_Platform=noaacloud.intel
+hpc_intel_ver=os.getenv("hpc_intel_ver") or "2021.3.0"
+load(pathJoin("stack-intel", hpc_intel_ver))
+
+hpc_impi_ver=os.getenv("hpc_impi_ver") or "2021.3.0"
+load(pathJoin("stack-intel-oneapi-mpi", hpc_impi_ver))
+
+load("ufs_common_spack")
+
+setenv("CC", "mpiicc")
+setenv("CXX", "mpiicpc")
+setenv("FC", "mpiifort")
+setenv("CMAKE_Platform", "noaacloud.intel")
+
+whatis("Description: UFS build environment")
