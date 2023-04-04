ed652156c9d107b02324fd6f55ce42a5979831f8
Bin Liu <Bin.Liu@noaa.gov>
2023-04-04 01:29:44 +0000

    Update submodule FV3. Update ufs_wcoss2.intel.lua and ufs_wcoss2.intel_debug.lua to load esmf/8.4.1, pio/2.5.10, and ip/4.0.0.

--- modulefiles/ufs_wcoss2.intel.lua
+++ modulefiles/ufs_wcoss2.intel.lua
@@ -48,7 +48,7 @@ load(pathJoin("g2", g2_ver))
 g2tmpl_ver=os.getenv("g2tmpl_ver") or "1.10.2"
 load(pathJoin("g2tmpl", g2tmpl_ver))
 
-ip_ver=os.getenv("ip_ver") or "3.3.3"
+ip_ver=os.getenv("ip_ver") or "4.0.0"
 load(pathJoin("ip", ip_ver))
 
 sp_ver=os.getenv("sp_ver") or "2.3.3"
@@ -57,26 +57,14 @@ load(pathJoin("sp", sp_ver))
 w3emc_ver=os.getenv("w3emc_ver") or "2.9.2"
 load(pathJoin("w3emc", w3emc_ver))
 
--- Second, look for libraries in "para"
-setenv("HPC_OPT", "/apps/ops/para/libs")
-prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304")
-prepend_path("MODULEPATH", "/apps/ops/para/libs/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.7")
+prepend_path("MODULEPATH", "/apps/dev/lmodules/intel/19.1.3.304")
+prepend_path("MODULEPATH", "/apps/dev/lmodules/intel_cray_mpich/19.1.3.304/cray-mpich/8.1.4")
 
-pio_ver=os.getenv("pio_ver") or "2.5.7"
+pio_ver=os.getenv("pio_ver") or "2.5.10"
 load(pathJoin("pio", pio_ver))
 
--- Finally, look for libraries in "dev" space
-prepend_path("MODULEPATH", "/apps/dev/lmodules/intel/19.1.3.304")
-prepend_path("MODULEPATH", "/apps/dev/modulefiles/mpi/intel/19.1.3.304/cray-mpich/8.1.9")
-
-gftl_shared_ver=os.getenv("gftl_shared_ver") or "1.5.0"
-load(pathJoin("gftl_shared", gftl_shared_ver))
-
-esmf_ver=os.getenv("esmf_ver") or "8.3.0b09"
+esmf_ver=os.getenv("esmf_ver") or "8.4.1"
 load(pathJoin("esmf", esmf_ver))
-
-mapl_ver=os.getenv("mapl_ver") or "2.23.1-esmf-8.3.0b09"
-load(pathJoin("mapl", mapl_ver))
 
 setenv("CC", "cc")
 setenv("CXX", "CC")
