50b0a02de669dd00bcb929902eb7d25d6c295069
JONG KIM <jong.kim@noaa.gov>
2023-05-04 17:04:47 -0400

    Update rt_utils.sh

--- tests/rt_utils.sh
+++ tests/rt_utils.sh
@@ -360,7 +360,7 @@ check_results() {
             printf ".......ALT CHECK.."
             if [[ ${MACHINE_ID} =~ orion || ${MACHINE_ID} =~ hera || ${MACHINE_ID} =~ gaea || ${MACHINE_ID} =~ jet || ${MACHINE_ID} =~ cheyenne ]] ; then
               nccmp -d -f -g -B --Attribute=checksum --warn=format ${RTPWD}/${CNTL_DIR}/${i} ${RUNDIR}/${i} > ${i}_nccmp.log 2>&1 && d=$? || d=$?
-              if [$d -ne 0] && [ $d -ne 1 ]; then
+              if [[ $d -ne 0 && $d -ne 1 ]]; then
 		  echo "....ERROR" >> ${REGRESSIONTEST_LOG}
 		  echo "....ERROR"
 		  exit 1
