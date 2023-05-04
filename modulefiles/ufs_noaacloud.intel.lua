b20f0b2a0c6a6064a2dfb4e25af37336e1426d50
Parallel Works app-run user <Cameron.Book@mgmt-cameronbook-pclusternoaav2use1-00037.pw-noaa-us-east-1.pw.local>
2023-05-04 21:52:34 +0000

    Update noaacloud.intel to use spack-stack/1.3.1.

--- modulefiles/ufs_noaacloud.intel.lua
+++ modulefiles/ufs_noaacloud.intel.lua
@@ -3,7 +3,8 @@ loads UFS Model prerequisites for NOAA Parallelworks/Intel
 ]])
 
 
-prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/spack-stack-1.3.0/envs/unified-dev/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/EPIC/spack-stack/spack-stack-1.3.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/modulefiles/core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.3.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -14,15 +15,14 @@ load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/modulefiles/core")
 stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
 load(pathJoin("stack-python", stack_python_ver))
 
 load("ufs_common")
 
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
+setenv("CC", "icc")
+setenv("CXX", "icpc")
+setenv("FC", "ifort")
 setenv("CMAKE_Platform", "noaacloud.intel")
 
 whatis("Description: UFS build environment")
