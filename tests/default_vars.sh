6d62b41508b30052e93a2b18a275fa00b5dc7094
denise.worthen <Denise.Worthen@noaa.gov>
2023-06-12 16:51:32 -0400

    re-work config options
    * set the ocean albedo limit in config and use it to set logical
    flag
    * albdir and albdif can be set as non-std values in nems.configure
    will default to 0.07 and 0.06 if not set

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -697,7 +697,7 @@ export ocn_model=mom6
 export ice_model=cice6
 export wav_model=ww3
 export pio_rearranger=box
-export limit_ocean_albedo=true
+export ocean_albedo_limit=0.06
 export use_mean_albedos=.false.
 export coupling_interval_slow_sec=${DT_THERM_MOM6}
 export coupling_interval_fast_sec=${DT_ATMOS}
@@ -950,7 +950,7 @@ export atm_model=datm
 export ocn_model=mom6
 export ice_model=cice6
 export pio_rearranger=box
-export limit_ocean_albedo=true
+export ocean_albedo_limit=0.06
 export use_mean_albedos=.false.
 
 export ATM_compute_tasks=$ATM_compute_tasks_cdeps_100
