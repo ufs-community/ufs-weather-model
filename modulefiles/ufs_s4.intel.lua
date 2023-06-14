e60a5998a7e51c8f9cc42659e8af2484771646ab
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-06-14 12:13:27 -0700

    Update ufs_s4.intel.lua

--- modulefiles/ufs_s4.intel.lua
+++ modulefiles/ufs_s4.intel.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for S4/Intel
 ]])
 
-prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack-1.3.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.4.0/envs/unified-env-v2/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/modulefiles")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
@@ -16,9 +16,9 @@ load(pathJoin("stack-python", stack_python_ver))
 
 load("ufs_common")
 
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
+setenv("CC", "icc")
+setenv("CXX", "icpc")
+setenv("FC", "ifort")
 setenv("CMAKE_Platform", "s4.intel")
 
 whatis("Description: UFS build environment")
