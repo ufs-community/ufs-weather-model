8e2218a2548e851ae4c04810df54313fc2fbad63
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-07-17 16:59:15 -0700

    Update stack path for hercules gnu modulefile

--- modulefiles/ufs_hercules.gnu.lua
+++ modulefiles/ufs_hercules.gnu.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for Hercules/GNU
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.4.1/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-dev-20230717/envs/unified-env/install/modulefiles/Core")
 
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "11.3.1"
 load(pathJoin("stack-gcc", stack_gnu_ver))
