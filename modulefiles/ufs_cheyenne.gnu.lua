90a31dd8e129e3c1afa23a8f8b7b5337cf5ca622
Cameron Book <43379611+ulmononian@users.noreply.github.com>
2023-08-21 16:52:44 -0700

    Revert ufs_cheyenne.gnu to hpc-stack version

--- modulefiles/ufs_cheyenne.gnu.lua
+++ modulefiles/ufs_cheyenne.gnu.lua
@@ -2,31 +2,44 @@ help([[
 loads UFS Model prerequisites for Cheyenne/GNU
 ]])
 
-unload("ncarenv/1.3")
-unload("intel/19.1.1")
-unload("ncarcompilers/0.5.0")
-unload("mpt/2.25")
-unload("netcdf/4.8.1")
-
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/cheyenne/spack-stack-1.4.1/envs/ufs-pio-2.5.10/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/glade/work/jedipara/cheyenne/spack-stack/modulefiles/misc")
-
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "10.1.0"
-load(pathJoin("stack-gcc", stack_gnu_ver))
-
-stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.1"
-load(pathJoin("stack-openmpi", stack_openmpi_ver))
-
-stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
-load(pathJoin("stack-python", stack_python_ver))
-
 cmake_ver=os.getenv("cmake_ver") or "3.22.0"
 load(pathJoin("cmake", cmake_ver))
+
+python_ver=os.getenv("python_ver") or "3.7.9"
+load(pathJoin("python", python_ver))
+
+ncarenv_ver=os.getenv("ncarenv_ver") or "1.3"
+load(pathJoin("ncarenv", ncarenv_ver))
+
+gnu_ver=os.getenv("gnu_ver") or "10.1.0"
+load(pathJoin("gnu", gnu_ver))
+
+mpt_ver=os.getenv("mpt_ver") or "2.25"
+load(pathJoin("mpt", mpt_ver))
+
+ncarcompilers_ver=os.getenv("ncarcompilers_ver") or "0.5.0"
+load(pathJoin("ncarcompilers", ncarcompilers_ver))
+
+unload("netcdf")
+
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/hpc-stack/gnu10.1.0_ncdf492/modulefiles/stack")
+
+hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+load(pathJoin("hpc", hpc_ver))
+
+hpc_gnu_ver=os.getenv("hpc_gnu_ver") or "10.1.0"
+load(pathJoin("hpc-gnu", hpc_gnu_ver))
+
+hpc_mpt_ver=os.getenv("hpc_mpt_ver") or "2.25"
+load(pathJoin("hpc-mpt", hpc_mpt_ver))
+
+scotch_ver=os.getenv("scotch_ver") or "7.0.3"
+load(pathJoin("scotch", scotch_ver))
 
 load("ufs_common")
 
 setenv("CC", "mpicc")
-setenv("CXX", "mpic++")
+setenv("CXX", "mpicxx")
 setenv("FC", "mpif90")
 setenv("CMAKE_Platform", "cheyenne.gnu")
 
