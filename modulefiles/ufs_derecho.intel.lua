6af7f80f944161b889646494d848fb15b99e86ad
Mark Potts <mark.potts@noaa.gov>
2023-08-31 14:36:39 -0600

    updated module files

--- modulefiles/ufs_derecho.intel.lua
+++ modulefiles/ufs_derecho.intel.lua
@@ -9,19 +9,22 @@ load("ecflow/5.8.4")
 load("mysql/8.0.33")
 
 setenv("LMOD_TMOD_FIND_FIRST","yes")
---prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/modulefiles_extra")
---prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-dev-20230825/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/modulefiles_extra")
+prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-dev-20230825/envs/unified-env/install/modulefiles/Core")
 --prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-dev-20230814/envs/unified-en2/install/modulefiles/Core")
 --prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-dev-20230814/envs/unified-env/install/modulefiles/Core")
 --prepend_path("MODULEPATH", "/lustre/desc1/scratch/mpotts/spack-stack/spack-stack-dev-20230814/envs/ufs-pio-2.5.10/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-1.4.1/envs/unified-dev/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/lustre/desc1/scratch/epicufsrt/contrib/spack-stack/spack-stack-1.4.1/envs/unified-dev/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.8.0"
---stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+--stack_intel_ver=os.getenv("stack_intel_ver") or "2021.8.0"
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
 load(pathJoin("stack-intel", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.8.0"
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+--stack_impi_ver=os.getenv("stack_impi_ver") or "2021.8.0"
+--load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+
+stack_impi_ver=os.getenv("stack_impi_ver") or "8.1.25"
+load(pathJoin("stack-cray-mpich", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.26.3"
 load(pathJoin("cmake", cmake_ver))
