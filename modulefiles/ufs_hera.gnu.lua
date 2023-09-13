cc4209370bf876cccd1c0ccb19add82adba9d060
Dom Heinzeller <dom.heinzeller@noaa.gov>
2023-09-13 14:17:39 +0000

    Update ufs_common.lua ufs_hera.gnu.lua ufs_hera.intel.lua

--- modulefiles/ufs_hera.gnu.lua
+++ modulefiles/ufs_hera.gnu.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for Hera/GNU
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/da/Dom.Heinzeller/spack-stack-esmf850/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/da/Dom.Heinzeller/spack-stack-esmf850/envs/unified-en2/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
 
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "9.2.0"
@@ -10,9 +10,6 @@ load(pathJoin("stack-gcc", stack_gnu_ver))
 
 stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.5"
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
-
-stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
-load(pathJoin("stack-python", stack_python_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
--- modulefiles/ufs_hera.intel.lua
+++ modulefiles/ufs_hera.intel.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for Hera/Intel
 ]])
 
-prepend_path("MODULEPATH", "/scratch1/NCEPDEV/da/Dom.Heinzeller/spack-stack-esmf850/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/da/Dom.Heinzeller/spack-stack-esmf850/envs/unified-en2/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/scratch1/NCEPDEV/jcsda/jedipara/spack-stack/modulefiles")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
@@ -10,9 +10,6 @@ load(pathJoin("stack-intel", stack_intel_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.1"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
-
-stack_python_ver=os.getenv("stack_python_ver") or "3.9.12"
-load(pathJoin("stack-python", stack_python_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
