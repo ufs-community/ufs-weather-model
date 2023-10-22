613fbb2ec6705cd9e2a04570e566d53315cb5908
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-10-22 08:27:42 -0600

    TEST: update modulefiles/ufs_hercules.gnu.lua

--- modulefiles/ufs_hercules.gnu.lua
+++ modulefiles/ufs_hercules.gnu.lua
@@ -2,15 +2,15 @@ help([[
 loads UFS Model prerequisites for Hercules/GNU
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
--- for openmpi, need:
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/modulefiles/")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env-gnu-mpich/install/modulefiles/Core")
+-- for mpich, need:
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/modulefiles")
 
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "11.3.1"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
-stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.5"
-load(pathJoin("stack-openmpi", stack_openmpi_ver))
+stack_mpich_ver=os.getenv("stack_mpich_ver") or "4.1.2"
+load(pathJoin("stack-mpich", stack_mpich_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
