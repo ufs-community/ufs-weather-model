d6bfc193c516ccd4212cd1aac7995d9d0fab7d51
Dom Heinzeller <dom.heinzeller@icloud.com>
2023-10-24 06:57:02 -0600

    Switch to mvapich2 on Hercules with GNU

--- modulefiles/ufs_hercules.gnu.lua
+++ modulefiles/ufs_hercules.gnu.lua
@@ -2,15 +2,15 @@ help([[
 loads UFS Model prerequisites for Hercules/GNU
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env-gnu-mpich/install/modulefiles/Core")
--- for mpich, need:
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env-mvap2/install/modulefiles/Core")
+-- for mvapich2, need:
 prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/modulefiles")
 
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "11.3.1"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
-stack_mpich_ver=os.getenv("stack_mpich_ver") or "4.1.2"
-load(pathJoin("stack-mpich", stack_mpich_ver))
+stack_mvapich2_ver=os.getenv("stack_mvapich2_ver") or "2.3.7"
+load(pathJoin("stack-mvapich2", stack_mvapich2_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
