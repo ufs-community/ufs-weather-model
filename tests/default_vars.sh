807526372727ac386d5a2eda2c41b91fad4802a6
Jessica Meixner <Jessica.Meixner@noaa.gov>
2023-11-03 13:31:20 +0000

    update datm MOM6 vars

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -1063,6 +1063,9 @@ export EPBL=-999.
 export MOM6_THERMO_SPAN=False
 export MOM6_USE_WAVES=False
 export MOM6_ALLOW_LANDMASK_CHANGES=False
+# MOM6 diag
+export MOM6_DIAG_COORD_DEF_Z_FILE=interpolate_zgrid_40L.nc
+export MOM6_DIAG_MISVAL='-1e34'
 
 # CICE6 defaults; 1 degree
 export MESHOCN_ICE=mesh.mx${OCNRES}.nc
