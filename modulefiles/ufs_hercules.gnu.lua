2212f504f8871497cd78fcd4fbb6737ed4eb6be5
Fernando Andrade - NOAA <121964418+FernandoAndrade-NOAA@users.noreply.github.com>
2023-12-14 09:53:24 -0700

    Update ufs_hercules.gnu.lua gcc for 1.5.1

--- modulefiles/ufs_hercules.gnu.lua
+++ modulefiles/ufs_hercules.gnu.lua
@@ -6,7 +6,7 @@ prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack
 -- for mvapich2, need:
 prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/modulefiles")
 
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "11.3.1"
+stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.2.0"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
 stack_mvapich2_ver=os.getenv("stack_mvapich2_ver") or "2.3.7"
