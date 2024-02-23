5f532d64d82b861704e3024dedbdc3d84dd1c6cc
Brian Curtis <brian.curtis@noaa.gov>
2024-02-23 19:53:54 +0000

    Bring in FV3 hash and .gitmodules changes

---
+++ tests/opnReqTests/bit.sh
@@ -0,0 +1,3 @@
+set -eu
+source $PATHRT/opnReqTests/std.sh
+source $PATHRT/opnReqTests/wrt_env.sh
---
+++ tests/opnReqTests/dcp.sh
@@ -0,0 +1,42 @@
+set -eu
+source $PATHRT/opnReqTests/std.sh
+
+if [[ $application == 'global' ]]; then
+  if [[ $CI_TEST == 'true' ]]; then
+    temp=$INPES
+    INPES=$JNPES
+    JNPES=$temp
+  else
+    temp=$INPES
+    INPES=$JNPES
+    JNPES=$temp
+  fi
+elif [[ $application == 'regional' ]]; then
+  if [[ $CI_TEST == 'true' ]]; then
+    INPES=10
+    JNPES=3
+    NTILES=1
+    TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP))
+    NODES=$(((TASKS+TPN-1)/TPN))
+  else
+    INPES=5
+    JNPES=12
+    NTILES=1
+  fi
+elif [[ $application == 'cpld' ]]; then
+  if [[ $CI_TEST == 'true' ]]; then
+    INPES=3
+    JNPES=1
+    OCN_tasks=10
+    ICE_tasks=6
+    NPROC_ICE=$ICE_tasks
+    TASKS=$((INPES*JNPES*NTILES + WRITE_GROUP*WRTTASK_PER_GROUP + OCN_tasks + ICE_tasks))
+  else
+    temp=$INPES
+    INPES=$JNPES
+    JNPES=$temp
+  fi
+fi
+
+(test $CI_TEST == 'true') && source $PATHRT/opnReqTests/cmp_proc_bind.sh
+source $PATHRT/opnReqTests/wrt_env.sh
