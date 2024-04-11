66e4a88f4ceb6ed5afddf9c110e7fc88cf3e180f
Brian Curtis <brian.curtis@noaa.gov>
2024-04-11 12:20:32 +0000

    WCOSS2 RT Log: Passed and Temp workaround to use hdf5 on WCOSS2 for CICE

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -663,6 +663,10 @@ export CICE_RESTART_CHUNK=0,0
 export CICE_RESTART_DEFLATE=0
 
 export CICE_HISTORY_FORMAT='pnetcdf2'
+if [[ ${MACHINE_ID} == wcoss2 ]]; then
+  export CICE_RESTART_FORMAT='hdf5'
+  export CICE_HISTORY_FORMAT='hdf5'
+fi
 export CICE_HISTORY_IOTASKS=-99
 export CICE_HISTORY_REARR='box'
 export CICE_HISTORY_ROOT=-99
