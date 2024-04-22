ca5c6ca86d63446028317510f3bc5f7b1be225eb
Jong Kim <jong.kim@noaa.gov>
2024-04-22 14:11:18 +0000

    add log script

---
+++ tests/create_log.py
@@ -0,0 +1,136 @@
+import os
+import sys
+import subprocess
+import yaml
+
+def get_testcase(test):
+    test_cases=[]
+    for case, configs in test.items():
+        case_name=case
+        case_config=configs
+        return case_name, case_config
+    
+def write_logfile(logfile, openmod, output="", subproc=""):
+    with open(logfile, openmod) as rtlog:
+        if (not subproc == "") :
+            subprocess.call(subproc, shell=True, stdout=rtlog)
+        if (not output == "") :
+            rtlog.writelines(output)
+    rtlog.close()
+            
+def finish_log():
+    PATHRT     = os.getenv('PATHRT')
+    MACHINE_ID = os.getenv('MACHINE_ID')
+    REGRESSIONTEST_LOG = PATHRT+'/logs/RegressionTests_'+MACHINE_ID+'.log'
+    filename   = REGRESSIONTEST_LOG
+
+    run_logs= f"""
+"""
+    JOB_NR  = 0
+    PASS_NR = 0
+    FAIL_NR = 0
+    with open("rt.yaml", 'r') as f:
+        rt_yaml = yaml.load(f)#, Loader=yaml.FullLoader)
+        for apps, jobs in rt_yaml.items():
+            for key, val in jobs.items():
+                if (str(key) == 'build'):
+                    if not ('turnoff' in val.keys()): print('   ',val['compiler'],val['option'])
+                    if 'turnoff' in val.keys(): print('   ',val['compiler'],val['option'],'turnoff: ',val['turnoff'])
+                    RT_COMPILER = val['compiler']
+                    COMPILE_ID  = apps+'_'+RT_COMPILER
+                    COMPILE_LOG = 'compile_'+COMPILE_ID+'.log'
+                    COMPILE_START_CHECK = "+ date_strt='"
+                    COMPILE_END_CHECK   = "+ date_end='"
+                    COMPILE_ELAPSE_CHECK= "+ echo 'Elapsed time"
+                    print('./logs/log_hera/'+COMPILE_LOG)
+                    with open('./logs/log_hera/'+COMPILE_LOG) as f:
+                        compilelog_file = f.readlines()
+                        for line in compilelog_file:
+                            if COMPILE_START_CHECK in line:
+                                sdate = line.split(' ')[4].strip()
+                                hh=sdate.split(":")[0]; mm=sdate.split(":")[1]; ss=sdate.split(":")[2]
+                                sdate_sec=int(hh) * 3600 + int(mm) * 60 + float(ss)
+                            if COMPILE_END_CHECK in line:
+                                edate = line.split(' ')[4].strip()
+                                hh=edate.split(":")[0]; mm=edate.split(":")[1]; ss=edate.split(":")[2]
+                                edate_sec=int(hh) * 3600 + int(mm) * 60 + float(ss)
+                            if COMPILE_ELAPSE_CHECK in line:
+                                ctime = line.split(' ')[4].strip()
+                    f.close()
+                    compile_log="PASS -- COMPILE "+COMPILE_ID+" ["+str(ctime)+"]\n"
+                    run_logs += compile_log
+                if (str(key) == 'tests'):
+                    JOB_NR+=1
+                    for test in val:
+                        case, config = get_testcase(test)
+                        TEST_NAME = case
+                        TEST_ID   = TEST_NAME+'_'+RT_COMPILER
+                        TEST_LOG  = 'rt_'+TEST_ID+'.log'
+                        if 'dependency' in config.keys():
+                            DEP_RUN = str(config['dependency'])+'_'+RT_COMPILER
+                        else:
+                            DEP_RUN = ""
+                        print(TEST_LOG)
+                        PASS_CHECK = 'Test '+TEST_ID+' PASS'
+                        TIME_CHECK = 'The total amount of wall time'
+                        MAXS_CHECK = 'The maximum resident set size (KB)'
+                        pass_flag = False
+                        with open('./logs/log_hera/'+TEST_LOG) as f:
+                            if PASS_CHECK in f.read():
+                                pass_flag = True
+                        f.close()
+                        with open('./logs/log_hera/'+TEST_LOG) as f:
+                            if pass_flag :
+                                rtlog_file = f.readlines()
+                                for line in rtlog_file:
+                                    if TIME_CHECK in line:
+                                        timing = line.split('=')[1].strip()
+                                        print(timing)
+                                    if MAXS_CHECK in line:
+                                        memsize= line.split('=')[1].strip()
+                                        print(memsize)
+                                test_log = 'PASS -- TEST '+TEST_ID+' ['+timing+']('+memsize+' MB)\n'
+                                PASS_NR += 1
+                            else:
+                                test_log = 'FAIL -- TEST '+TEST_ID+'\n'
+                                FAIL_NR += 1
+                            run_logs += test_log
+                        f.close()
+    write_logfile(filename, "a", output=run_logs)
+
+    TEST_START_TIME= str(1111)
+    TEST_END_TIME  = str(2222)
+    elapsed_time   = str(1111)
+    COMPILE_PASS   = str(11)
+    COMPILE_NR     = str(222)
+    JOB_NR         = str(222)
+    PASS_NR        = str(111)
+    FAIL_NR        = str(111)
+    synop_log = f"""
+SYNOPSIS:
+Starting Date/Time: {TEST_START_TIME}
+Ending Date/Time: {TEST_END_TIME}
+Total Time: {elapsed_time}
+Compiles Completed: {COMPILE_PASS}/{COMPILE_NR}
+Tests Completed: {PASS_NR}/{JOB_NR}
+
+"""    
+    write_logfile(filename, "a", output=synop_log)
+
+    if (JOB_NR == PASS_NR):
+        SUCCESS = "SUCCESS"
+    else:
+        SUCCESS = "FAILED"
+        
+    comment_log = f"""NOTES:
+A file 'test_changes.list' was generated but is empty.
+If you are using this log as a pull request verification, please commit 'test_changes.list'.
+
+Result: {SUCCESS}
+
+====END OF {MACHINE_ID} REGRESSION TESTING LOG====
+"""
+    write_logfile(filename, "a", output=comment_log)
+    
+if __name__ == '__main__':
+    finish_log()
--- tests/ufs_test.sh
+++ tests/ufs_test.sh
@@ -31,138 +31,6 @@ usage() {
 
 [[ $# -eq 0 ]] && usage
 
-update_rtconf() {
-
-  find_match() {
-    # This function finds if a test in $TESTS_FILE matches one 
-    # in our list of tests to be run.
-    THIS_TEST_WITH_COMPILER=$1
-    shift
-    TWC=("$@")
-    FOUND=false
-    for i in "${!TWC[@]}"; do
-      if [[ "${TWC[$i]}" == "${THIS_TEST_WITH_COMPILER}" ]]; then
-        FOUND=true
-        echo "${i}"
-        return
-      fi
-    done
-    if [[ $FOUND == false ]]; then
-      echo "-1"
-    fi
-  }
-
-  # This script will update the rt.conf ($TESTS_FILE) if needed by the
-  # -b or -n options being called/used.
-
-  # THE USER CHOSE THE -b OPTION
-  if [[ $NEW_BASELINES_FILE != '' ]]; then
-    [[ -s "$NEW_BASELINES_FILE" ]] || die "${NEW_BASELINES_FILE} is empty, exiting..."
-    TEST_WITH_COMPILE=()
-    readarray -t TEST_WITH_COMPILE < "$NEW_BASELINES_FILE"
-  # else USER CHOSE THE -l OPTION
-  elif [[ $DEFINE_CONF_FILE == true ]]; then
-    echo "No update needed to TESTS_FILE"
-    return
-  # else USER CHOSE THE -n OPTION
-  elif [[ $RUN_SINGLE_TEST == true ]]; then
-    TEST_WITH_COMPILE=("${SRT_NAME} ${SRT_COMPILER}")
-  else
-    echo "No update needed to rt.conf"
-    return
-  fi
-
-  RT_TEMP_CONF="rt_temp.conf"
-  rm -f $RT_TEMP_CONF && touch $RT_TEMP_CONF
-  local compile_line=''
-  
-  while read -r line || [ "$line" ]; do
-    line="${line#"${line%%[![:space:]]*}"}"
-    [[ -n $line ]] || continue
-    [[ ${#line} == 0 ]] && continue
-    [[ $line == \#* ]] && continue
-    
-    if [[ $line =~ COMPILE ]] ; then
-      MACHINES=$(echo $line | cut -d'|' -f5 | sed -e 's/^ *//' -e 's/ *$//')
-      RT_COMPILER_IN=$(echo $line | cut -d'|' -f3 | sed -e 's/^ *//' -e 's/ *$//')
-      if [[ ${MACHINES} == '' ]]; then
-        compile_line=$line
-        COMPILE_LINE_USED=false
-      elif [[ ${MACHINES} == -* ]]; then
-        [[ ${MACHINES} =~ ${MACHINE_ID} ]] || compile_line=$line; COMPILE_LINE_USED=false
-      elif [[ ${MACHINES} == +* ]]; then
-        [[ ${MACHINES} =~ ${MACHINE_ID} ]] && compile_line=$line; COMPILE_LINE_USED=false
-      fi
-
-    fi
-
-    if [[ $line =~ RUN ]]; then
-      to_run_test=false
-      tmp_test=$(echo "$line" | cut -d'|' -f2 | sed -e 's/^ *//' -e 's/ *$//')
-      MACHINES=$(echo $line | cut -d'|' -f3 | sed -e 's/^ *//' -e 's/ *$//')
-      if [[ ${MACHINES} == '' ]]; then
-        to_run_test=true
-      elif [[ ${MACHINES} == -* ]]; then
-        [[ ${MACHINES} =~ ${MACHINE_ID} ]] || to_run_test=true
-      elif [[ ${MACHINES} == +* ]]; then
-        [[ ${MACHINES} =~ ${MACHINE_ID} ]] && to_run_test=true
-      fi
-      if [[ $to_run_test == true ]]; then
-        TEST_IDX=$(find_match "$tmp_test $RT_COMPILER_IN" "${TEST_WITH_COMPILE[@]}")
-        
-        if [[ $TEST_IDX != -1 ]]; then
-          if [[ $COMPILE_LINE_USED == false ]]; then
-              echo -en '\n' >> $RT_TEMP_CONF
-            echo "$compile_line" >> $RT_TEMP_CONF
-            COMPILE_LINE_USED=true
-          fi
-          dep_test=$(echo "$line" | grep -w "$tmp_test" | cut -d'|' -f5 | sed -e 's/^ *//' -e 's/ *$//')
-        
-          if [[ $dep_test != '' ]]; then
-            if [[ $(find_match "$dep_test $RT_COMPILER_IN" "${TEST_WITH_COMPILE[@]}") == -1 ]]; then
-  
-              dep_line=$(grep -w "$dep_test" rt.conf | grep -v "$tmp_test")
-              dep_line="${dep_line#"${dep_line%%[![:space:]]*}"}"
-              dep_line=$(echo "${dep_line}" | tr -d '\n')
-              CORRECT_LINE[1]=$(awk -F'RUN|RUN' '{print $2}' <<< "$dep_line")
-              CORRECT_LINE[2]=$(awk -F'RUN|RUN' '{print $3}' <<< "$dep_line")
-            
-              if [[ $RT_COMPILER_IN == "intel" ]]; then
-                echo "RUN ${CORRECT_LINE[1]}" >> $RT_TEMP_CONF
-              elif [[ $RT_COMPILER_IN == "gnu" ]]; then
-                echo "RUN ${CORRECT_LINE[2]}" >> $RT_TEMP_CONF
-              fi
-            fi
-          fi
-          echo "$line" >> $RT_TEMP_CONF
-        fi
-      fi
-    fi
-  done < "$TESTS_FILE"
-
-  if [[ ! -s $RT_TEMP_CONF ]]; then
-    echo "The tests listed/chosen do not exist or cannot be run on $MACHINE_ID"
-    exit 1
-  else
-    TESTS_FILE=$RT_TEMP_CONF
-  fi
-}
-
-rt_35d() {
-if [[ $TEST_NAME =~ '35d' ]] ; then
-  local sy=$(echo ${DATE_35D} | cut -c 1-4)
-  local sm=$(echo ${DATE_35D} | cut -c 5-6)
-  local new_test_name="tests/${TEST_NAME}_${DATE_35D}"
-  rm -f $new_test_name
-  cp tests/$TEST_NAME $new_test_name
-
-  sed -i -e "s/\(export SYEAR\)/\1=\"$sy\"/" $new_test_name
-  sed -i -e "s/\(export SMONTH\)/\1=\"$sm\"/" $new_test_name
-
-  TEST_NAME=${new_test_name#tests/}
-fi
-}
-
 rt_trap() {
   [[ ${ROCOTO:-false} == true ]] && rocoto_kill
   [[ ${ECFLOW:-false} == true ]] && ecflow_kill
@@ -367,6 +235,16 @@ declare -A compiles
     export CREATE_BASELINE
     #RTVERBOSE = false
     export RTVERBOSE
+
+    export TESTS_FILE
+    export SINGLE_OPTS
+    export NEW_BASELINES_FILE
+    export DEFINE_CONF_FILE
+    export RUN_SINGLE_TEST
+    export COMPILE_ONLY
+    export delete_rundir
+    export skip_check_results
+    export KEEP_RUNDIR    
     
     python -c "import create_xml; create_xml.main_loop()"
 
@@ -389,3 +267,4 @@ fi
 
 ## Lets verify all tests were run and that they passed
 #generate_log
+python -c "import create_log; create_log.finish_log()"
