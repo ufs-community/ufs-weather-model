d3446b37c1eb1a1a99025038e688ed2bbe74fb10
Wei Huang <Wei.Huang@noaa.gov>
2024-04-26 14:11:10 +0000

    add ufs_common4noaacloud.lua

--- modulefiles/ufs_common4noaacloud.lua
+++ modulefiles/ufs_common4noaacloud.lua
@@ -1,27 +1,31 @@
-whatis("Description: UFS build environment common libraries")
+whatis("Description: UPP build environment common libraries")
 
 help([[Load UFS Model common libraries]])
 
 local ufs_modules = {
+  {["esmf"]            = "8.6.0"},
+  {["fms"]             = "2023.04"},
+  {["gftl"]            = "1.10.0"},
+  {["gftl-shared"]     = "1.6.1"},
   {["jasper"]          = "2.0.32"},
-  {["zlib"]            = "1.2.13"},
+  {["mapl"]            = "2.40.3-esmf-8.6.0"},
   {["libpng"]          = "1.6.37"},
   {["hdf5"]            = "1.14.0"},
   {["netcdf-c"]        = "4.9.2"},
-  {["netcdf-fortran"]  = "4.6.0"},
+  {["netcdf-fortran"]  = "4.6.1"},
   {["parallelio"]      = "2.5.10"},
-  {["esmf"]            = "8.5.0"},
-  {["fms"]             = "2023.02.01"},
   {["bacio"]           = "2.4.1"},
-  {["crtm"]            = "2.4.0"},
+  {["crtm"]            = "2.4.0.1"},
   {["g2"]              = "3.4.5"},
   {["g2tmpl"]          = "1.10.2"},
   {["ip"]              = "4.3.0"},
-  {["sp"]              = "2.3.3"},
+  {["sp"]              = "2.5.0"},
   {["w3emc"]           = "2.10.0"},
-  {["gftl-shared"]     = "1.6.1"},
-  {["mapl"]            = "2.40.3-esmf-8.5.0"},
-  {["scotch"]          = "7.0.4"},
+  {["nemsio"]          = "2.5.4"},
+  {["sigio"]           = "2.3.2"},
+  {["sfcio"]           = "1.4.1"},
+  {["wrf-io"]          = "1.2.0"},
+  {["zlib"]            = "1.2.13"},
 }
 
 for i = 1, #ufs_modules do
