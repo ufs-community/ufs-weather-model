8b9f1629789e0be897c2783f28ba2941cc664d7a
David Burrows <David.Burrows@noaa.gov>
2024-05-06 10:21:40 -0400

    update ufs_gaea.intel.lua for building ufs_weather_model on Gaea

--- modulefiles/ufs_gaea.intel.lua
+++ modulefiles/ufs_gaea.intel.lua
@@ -1,17 +1,8 @@
 help([[
-  This module loads libraries required for building and running UFS Weather Model 
-  on the NOAA RDHPC machine Gaea C5 using Intel-2023.1.0.
+loads UFS Model prerequisites for Gaea/Intel
 ]])
 
-whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea ]===])
-
-load("PrgEnv-intel/8.3.3")
-load("intel-classic/2023.1.0")
-load("cray-mpich/8.1.25")
-load("python/3.9.12")
-
 prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/spack-stack-1.5.1/envs/unified-env/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/ncrc/proj/epic/spack-stack/modulefiles")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2023.1.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -19,16 +10,22 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
-load(pathJoin("stack-python", stack_python_ver))
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
-load("nccmp/1.9.0.1")
 
-unload("darshan-runtime")
-unload("cray-libsci")
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
+load(pathJoin("nccmp", nccmp_ver))
 
 setenv("CC","cc")
 setenv("CXX","CC")
 setenv("FC","ftn")
 setenv("CMAKE_Platform","gaea.intel")
+-- failed with:Could not find compiler set in environment variable CC:
+--setenv("CC", "mpiicc")
+--setenv("CXX", "mpiicpc")
+--setenv("FC", "mpiifort")
+--setenv("CMAKE_Platform", "gaea.intel")
+
+whatis("Description: UFS build environment")
