84c31b4dbbfddb27826b5e918093b90ba3c89070
David Burrows <David.Burrows@noaa.gov>
2024-05-07 11:54:45 -0400

    add stack-python load back to ufs_gaea.intel.lua

--- modulefiles/ufs_gaea.intel.lua
+++ modulefiles/ufs_gaea.intel.lua
@@ -13,6 +13,9 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.25"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
+stack_python_ver=os.getenv("stack_python_ver") or "3.10.8"
+load(pathJoin("stack-python", stack_python_ver))
+
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
