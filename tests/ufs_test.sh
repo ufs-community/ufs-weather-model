b8f72b943a39e3230d5a311120c437677ab53c87
JONG KIM <jong.kim@noaa.gov>
2024-05-12 10:41:35 -0400

    Update ufs_test.sh

--- tests/ufs_test.sh
+++ tests/ufs_test.sh
@@ -231,6 +231,9 @@ export skip_check_results
 export KEEP_RUNDIR  
 
 python -c "import create_xml; create_xml.main_loop()"
+if [[ $? != 0]]; then
+    echo "*** experiment setup didn't run successfully! ***"
+fi
 
 ##
 ## run regression test workflow (currently Rocoto or ecFlow are supported)
