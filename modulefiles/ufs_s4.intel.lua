74f0ac5037d24dc6b6994e0f41448ac651f0594f
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-06-12 09:11:34 -0600

    Temporary change: use intel@19.0.5 on S4

--- modulefiles/ufs_s4.intel.lua
+++ modulefiles/ufs_s4.intel.lua
@@ -2,13 +2,13 @@ help([[
 loads UFS Model prerequisites for S4/Intel
 ]])
 
-prepend_path("MODULEPATH", "/data/prod/jedi/spack-stack/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/data/users/dheinzeller/ufs-wm-chunked/spst151-i19/envs/unified-env/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.5.0"
+stack_intel_ver=os.getenv("stack_intel_ver") or "2019.5.281"
 load(pathJoin("stack-intel", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.5.0"
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
+stack_impi_ver=os.getenv("stack_impi_ver") or "2019.5.281"
+load(pathJoin("stack-intel-mpi", stack_impi_ver))
 
 load("ufs_common")
 
