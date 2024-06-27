e536905d6fda031882ebc88aa08b6447edf42e00
samuel.trahan <Samuel.Trahan@noaa.gov>
2024-06-27 20:28:57 +0000

    correct a comment

--- tests/rt_utils.sh
+++ tests/rt_utils.sh
@@ -17,8 +17,8 @@ jobid=0
 redirect_out_err() {
     ( set -e -o pipefail ; ( "$@" 2>&1 1>&3 3>&- | tee err ) 3>&1 1>&2 | tee out )
     # The above shell redirection copies stdout to "out" and stderr to "err"
-    # while still sending them to stdout and stderr. It does this without
-    # relying on bash-specific extensions or non-standard OS features.
+    # while still sending them to stdout and stderr. It ensures the entire
+    # redirect_out_err command will return non-zero if "$@" or tee return non-zero.
 }
 
 function compute_petbounds_and_tasks() {
