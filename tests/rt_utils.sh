e8819dcca280694e58a6e5f8dda95b7292955dde
Dusan Jovic <dusan.jovic@noaa.gov>
2024-07-08 15:13:16 +0000

    Update rt_utils.sh. Set ECF_HOST on wcoss2 explicitly

--- tests/rt_utils.sh
+++ tests/rt_utils.sh
@@ -616,16 +616,13 @@ ecflow_run() {
   # Make sure ECF_HOST and ECF_PORT are set/ready on systems that have an
   # explicit ecflow node
   if [[ ${MACHINE_ID} == wcoss2 || ${MACHINE_ID} == acorn ]]; then
-    readarray -t ECFHOSTLIST < "${ECF_HOSTFILE}"
-    for ECF_HOST in "${ECFHOSTLIST[@]}"
-    do
-      if ssh -q "${ECF_HOST}" "exit"; then
-        export ECF_HOST
-        break
-      else
-        ECF_HOST=''
-      fi
-    done
+    if [[ "${HOST::1}" == "a" ]]; then
+      ECF_HOST=aecflow01
+    elif [[ "${HOST::1}" == "c" ]]; then
+      ECF_HOST=cdecflow01
+    elif [[ "${HOST::1}" == "d" ]]; then
+      ECF_HOST=ddecflow01
+    fi
   elif [[ ${MACHINE_ID} == hera || ${MACHINE_ID} == jet ]]; then
     module load ecflow
   fi
