4000d72918e56db588bf78683fcb1560866ae4ad
Nick Szapiro <nick.szapiro@noaa.gov>
2024-08-13 11:13:58 -0500

    Remove gfs_hafs from CDEPS-interface/cdeps_files.cmake

--- CDEPS-interface/cdeps_files.cmake
+++ CDEPS-interface/cdeps_files.cmake
@@ -46,7 +46,6 @@ list(APPEND cdeps_datm_files
   CDEPS/datm/datm_datamode_cplhist_mod.F90
   CDEPS/datm/datm_datamode_era5_mod.F90
   CDEPS/datm/datm_datamode_gefs_mod.F90
-  CDEPS/datm/datm_datamode_gfs_hafs_mod.F90
   CDEPS/datm/datm_datamode_jra_mod.F90
   CDEPS/datm/datm_datamode_simple_mod.F90
 )
