55ceceb9180d6733008dc5b01e5cc7b5b36271dd
Wei Huang <Wei.Huang@noaa.gov>
2024-10-03 14:29:33 +0000

    Compile ufs-weather-model successfully on GCP with Rocky 8

--- modulefiles/ufs_noaacloud.intel.lua
+++ modulefiles/ufs_noaacloud.intel.lua
@@ -2,17 +2,11 @@ help([[
 loads UFS Model prerequisites for NOAA Parallelworks/Intel
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/upp-addon-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/apps/modules/modulefiles")
 load("gnu")
 load("stack-intel")
 load("stack-intel-oneapi-mpi")
-
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-load(pathJoin("stack-intel", stack_intel_ver))
-
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.10.0"
-load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 unload("gnu")
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
