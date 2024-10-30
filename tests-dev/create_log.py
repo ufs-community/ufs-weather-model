5d69998dadcbb46baf0a285bcdb15cc1ac639123
Natalie Perlin <Natalie.Perlin@noaa.gov>
2024-10-30 01:09:23 +0000

    HSD testing for noaacloud

--- tests-dev/create_log.py
+++ tests-dev/create_log.py
@@ -145,8 +145,15 @@ def finish_log():
 
     TEST_START_TIME, TEST_END_TIME = get_timestamps('./logs/log_'+MACHINE_ID+'/')
     
-    start_time      = datetime.strptime(TEST_START_TIME, "%Y-%m-%d %H:%M:%S")
-    end_time        = datetime.strptime(TEST_END_TIME, "%Y-%m-%d %H:%M:%S")
+    clean_START_TIME= TEST_START_TIME.split('.')[0]
+    start_time      = datetime.strptime(clean_START_TIME, "%Y-%m-%d %H:%M:%S")
+    clean_END_TIME= TEST_END_TIME.split('.')[0]
+    end_time        = datetime.strptime(clean_END_TIME, "%Y-%m-%d %H:%M:%S")
+    print(TEST_START_TIME)
+    print(clean_START_TIME)
+    print(TEST_END_TIME)
+    print(clean_END_TIME)
+
     hours, remainder= divmod((end_time - start_time).total_seconds(), 3600)
     minutes, seconds= divmod(remainder, 60)
     hours = int(hours);    minutes=int(minutes);     seconds =int(seconds)
