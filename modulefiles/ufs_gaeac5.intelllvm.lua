dba7cbf7f0f774c87d31ac7a7cfdca91b7ed61ca
jkbk2004 <jong.kim@noaa.gov>
2024-12-18 10:14:04 -0500

    revert back complier version update on gaeac5: RT pased

--- modulefiles/ufs_gaeac5.intelllvm.lua
+++ modulefiles/ufs_gaeac5.intelllvm.lua
@@ -1,6 +1,6 @@
 help([[
   This module loads libraries required for building and running UFS Weather Model
-  on the NOAA RDHPC machine Gaea C5 using Intel-2023.2.0.
+  on the NOAA RDHPC machine Gaea C5 using Intel-2023.1.0.
 ]])
 
 whatis([===[Loads libraries needed for building the UFS Weather Model on Gaea C5]===])
@@ -27,8 +27,7 @@ load(pathJoin("nccmp", nccmp_ver))
 unload("darshan-runtime")
 unload("cray-libsci")
 
-unload("intel-classic/2023.2.0")
-load("intel-oneapi/2023.2.0")
+load("intel-classic/2023.2.0")
 
 setenv("I_MPI_CC", "icx")
 setenv("I_MPI_CXX", "icpx")
