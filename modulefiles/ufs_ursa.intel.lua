8149b8ef91c50f715a4cfb60c9cef13d3e2066c7
ulmononian <cameron.book@noaa.gov>
2025-01-27 23:51:07 +0000

    Update Ursa modulefile again.

--- modulefiles/ufs_ursa.intel.lua
+++ modulefiles/ufs_ursa.intel.lua
@@ -2,9 +2,6 @@ help([[
 loads UFS Model prerequisites for Ursa/Intel
 ]])
 
---prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/Core")
---prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-eaajhcw/oneapi/2024.2.1")
-
 prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.6.0/fms-2024.01/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
@@ -20,10 +17,6 @@ load("ufs_common")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
 load(pathJoin("nccmp", nccmp_ver))
-
-setenv("CC", "icc")
-setenv("CXX", "icpc")
-setenv("FC", "ifort")
 
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
