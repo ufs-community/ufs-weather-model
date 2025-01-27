bafc404ac4f0fef62806ce7d6e88221e285991a3
ulmononian <cameron.book@noaa.gov>
2025-01-27 23:49:52 +0000

    Update Ursa modulefile,

--- modulefiles/ufs_ursa.intel.lua
+++ modulefiles/ufs_ursa.intel.lua
@@ -2,16 +2,18 @@ help([[
 loads UFS Model prerequisites for Ursa/Intel
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-eaajhcw/oneapi/2024.2.1")
+--prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.8.0/ue-oneapi-ifort-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-eaajhcw/oneapi/2024.2.1")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
-load(pathJoin("stack-oneapi", stack_intel_ver))
+prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.6.0/fms-2024.01/install/modulefiles/Core")
+
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+load(pathJoin("stack-intel", stack_intel_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+cmake_ver=os.getenv("cmake_ver") or "3.23.1"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
@@ -19,11 +21,16 @@ load("ufs_common")
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
 load(pathJoin("nccmp", nccmp_ver))
 
+setenv("CC", "icc")
+setenv("CXX", "icpc")
+setenv("FC", "ifort")
+
 setenv("CC", "mpiicc")
 setenv("CXX", "mpiicpc")
 setenv("FC", "mpiifort")
-setenv("I_MPI_CC", "icx")
-setenv("I_MPI_CXX", "icpx")
+
+setenv("I_MPI_CC", "icc")
+setenv("I_MPI_CXX", "icpc")
 setenv("I_MPI_F90", "ifort")
 
 setenv("CMAKE_Platform", "ursa.intel")
