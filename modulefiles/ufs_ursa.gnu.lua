0254f1db83ad3e64c2e7be08dcf507e494564dcd
ulmononian <cameron.book@noaa.gov>
2025-02-10 18:44:49 +0000

    Update Ursa GNU modulefile.

--- modulefiles/ufs_ursa.gnu.lua
+++ modulefiles/ufs_ursa.gnu.lua
@@ -2,7 +2,12 @@ help([[
 loads UFS Model prerequisites for Ursa/GNU
 ]])
 
-prepend_path("MODULEPATH", "/collab1/data/Ratko.Vasic/spack-stack-1.6.0/envs/fms-2024.01-gnu/install/modulefiles/Core")
+-- ratko custom install
+--prepend_path("MODULEPATH", "/collab1/data/Ratko.Vasic/spack-stack-1.6.0/envs/fms-2024.01-gnu/install/modulefiles/Core")
+
+-- sysadmin install
+prepend_path("MODULEPATH", "/collab1/data/Ratko.Vasic/spack-stack-1.6.0/envs/fms-2024.01-gnu-sys/install/modulefiles/Core")
+
 
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.4.0"
 load(pathJoin("stack-gcc", stack_gnu_ver))
@@ -18,9 +23,9 @@ load("ufs_common")
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
 load(pathJoin("nccmp", nccmp_ver))
 
-setenv("CC", "mpicc")
-setenv("CXX", "mpic++")
-setenv("FC", "mpif90")
+setenv("MPI_CC", "mpicc")
+setenv("MPI_CXX", "mpic++")
+setenv("MPI_FC", "mpifort")
 
 setenv("CMAKE_Platform", "ursa.gnu")
 
