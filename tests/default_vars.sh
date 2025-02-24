a946ce4019e16713fe7ce61aee8a6c5c2c3b039e
denise.worthen <Denise.Worthen@noaa.gov>
2025-02-24 16:41:17 +0000

    fix TILEDFIX variable for cpld tests
    * set tiledfix to true and add copy-ins of req'd grb files

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -1474,7 +1474,7 @@ export DIAG_TABLE=diag_table_cpld.IN
 export DIAG_TABLE_ADDITIONAL=''
 export FIELD_TABLE_ADDITIONAL=''
 export FV3_RUN=cpld_control_run.IN
-export TILEDFIX=.false.
+export TILEDFIX=.true.
 
 export FHZERO=6
 
