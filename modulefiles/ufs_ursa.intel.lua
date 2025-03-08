82e25f6138c4104c2d4916078276765ae83578ba
ulmononian <cameron.book@noaa.gov>
2025-03-08 22:17:16 +0000

    Update Ursa intel modulefile and ufs_common for spack-stack 1.9.1 on Ursa Oso test system.

--- modulefiles/ufs_ursa.intel.lua
+++ modulefiles/ufs_ursa.intel.lua
@@ -2,15 +2,16 @@ help([[
 loads UFS Model prerequisites for Ursa/Intel
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.6.0/fms-2024.01/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/contrib/spack-stack/envs/1.6.0/fms-2024.01/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
@@ -18,13 +19,13 @@ load("ufs_common")
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
 load(pathJoin("nccmp", nccmp_ver))
 
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
-
-setenv("I_MPI_CC", "icc")
-setenv("I_MPI_CXX", "icpc")
-setenv("I_MPI_F90", "ifort")
+--setenv("CC", "mpiicc")
+--setenv("CXX", "mpiicpc")
+--setenv("FC", "mpiifort")
+--
+--setenv("I_MPI_CC", "icc")
+--setenv("I_MPI_CXX", "icpc")
+--setenv("I_MPI_F90", "ifort")
 
 setenv("CMAKE_Platform", "ursa.intel")
 
