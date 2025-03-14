08b34714010c20491b5e58efcd42e9952b68bd27
Ratko Vasic <Ratko.Vasic@noaa.gov>
2025-03-13 19:37:46 -0500

    Add Orion modulefile

--- modulefiles/ufs_orion.intel.lua
+++ modulefiles/ufs_orion.intel.lua
@@ -2,21 +2,21 @@ help([[
 loads UFS Model prerequisites for Orion/Intel
 ]])
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/orion/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-load(pathJoin("stack-intel", stack_intel_ver))
+stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+load(pathJoin("stack-oneapi", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
 
 -- HDF5 needed for LM4
-hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
+hdf5_ver=os.getenv("hdf5_ver") or "1.14.3"
 load(pathJoin("hdf5", hdf5_ver))
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
