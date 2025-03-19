5c5642cf0c938c6b808d0addda1d9117309a4993
ulmononian <cameron.book@noaa.gov>
2025-03-19 21:04:05 +0000

    Update Ursa GNU modulefile.

--- modulefiles/ufs_ursa.gnu.lua
+++ modulefiles/ufs_ursa.gnu.lua
@@ -2,15 +2,15 @@ help([[
 loads UFS Model prerequisites for Ursa/GNU
 ]])
 
-prepend_path("MODULEPATH", "/collab1/data/Ratko.Vasic/spack-stack-1.6.0/envs/fms-2024.01-gnu-sys/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.1/envs/ue-gcc-11.4.1/install/modulefiles/Core")
 
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.4.0"
+stack_gnu_ver=os.getenv("stack_gnu_ver") or "11.4.1"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
 stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.6"
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.23.1"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
