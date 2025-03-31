d1636b3d348f2785ed652ff3bd6c73e570d4b881
ulmononian <cameron.book@noaa.gov>
2025-03-31 15:58:56 +0000

    Update DISKNM.

--- tests/rt.sh
+++ tests/rt.sh
@@ -809,8 +809,8 @@ case ${MACHINE_ID} in
     COMPILE_QUEUE="batch"
 
     PARTITION="compute"
-    dprefix="//tds_scratch2/SYSADMIN/pilot-users/${USER}"
-    DISKNM="/tds_scratch2/SYSADMIN/pilot-users/role.epic/NEMSfv3gfs"
+    dprefix="/tds_scratch2/SYSADMIN/pilot-users/${USER}"
+    DISKNM="/tds_scratch2/SYSADMIN/pilot-users/role.epic/UFS-WM_RT"
     STMP="${STMP:-${dprefix}/RT_BASELINE}"
     PTMP="${PTMP:-${dprefix}/RT_RUNDIRS}"
 
