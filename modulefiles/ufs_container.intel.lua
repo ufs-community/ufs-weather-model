b92a89a07bb59c70fe7c03a2dbbef2c3d0fd345e
Wei Huang <Wei.Huang@noaa.gov>
2025-04-14 16:15:01 +0000

    fix detect issue in container

--- modulefiles/ufs_container.intel.lua
+++ modulefiles/ufs_container.intel.lua
@@ -1,37 +1,27 @@
 help([[
-loads UFS Model prerequisites for Container
+Load environment to compile ufs-weather-model in a container using Intel
 ]])
 
-prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/fms-2024.01/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/root/modulefiles")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.9.0/intel/2021.10.0")
 
-local stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
-local stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
-local impi_ver=os.getenv("impi_ver") or "2021.11"
+stack_intel_ver=os.getenv("stack_intel_ver") or "2021.10.0"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 
 load("gnu")
 load(pathJoin("stack-intel", stack_intel_ver))
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 unload("gnu")
 
-load("tbb/latest")
-load("compiler-rt/latest")
-load("oclfpga/latest")
-load("compiler/latest")
-load("mpi/latest")
-
-local cmake_ver=os.getenv("cmake_ver") or "3.23.1"
-load(pathJoin("cmake", cmake_ver))
-
 load("ufs_common")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
 
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
+-- setenv("CC", "mpiicc")
+-- setenv("CXX", "mpiicpc")
+-- setenv("FC", "mpiifort")
 setenv("CMAKE_Platform", "container.intel")
 
 whatis("Description: UFS build environment")
