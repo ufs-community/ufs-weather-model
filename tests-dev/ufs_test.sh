29e20b7df4841e8f25550291fdf5a94afc1af145
ulmononian <cameron_book@alumni.brown.edu>
2025-04-14 12:31:18 -0500

    Add field_table/diag_table parm dirs, tc field/diag tables, tc fd_ufs.yaml; update ufs_test.sh.

--- tests-dev/ufs_test.sh
+++ tests-dev/ufs_test.sh
@@ -212,6 +212,9 @@ if [[ ${LINK_TESTS} == true ]]; then
     cp "${PATHRT}"/test_cases/tests/* "${PATHRT}"/tests
     cp "${PATHRT}"/test_cases/exp_conf/* "${PATHRT}"/fv3_conf
     cp "${PATHRT}"/test_cases/parm/* "${PATHRT}"/parm
+    cp "${PATHRT}"/test_cases/parm/diag_table/* "${PATHRT}"/parm/diag_table
+    cp "${PATHRT}"/test_cases/parm/field_table/* "${PATHRT}"/parm/field_table
+
 fi
 
 #Check to error out if incompatible options are chosen together
