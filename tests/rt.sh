2a0601ac4b54bf5a538859426edb71907f0456c7
ulmononian <cameron.book@noaa.gov>
2025-04-29 17:32:11 +0000

    Update rt.sh and fv3_conf files for Ursa (partition name changed from compute to ursa).

--- tests/rt.sh
+++ tests/rt.sh
@@ -808,7 +808,7 @@ case ${MACHINE_ID} in
     QUEUE="batch"
     COMPILE_QUEUE="batch"
 
-    PARTITION="compute"
+    PARTITION="ursa"
     #dprefix="/tds_scratch2/SYSADMIN/pilot-users/${USER}"
     #DISKNM="/tds_scratch2/SYSADMIN/pilot-users/role.epic/UFS-WM_RT"
     dprefix="/scratch4/NAGAPE/epic/${USER}"
