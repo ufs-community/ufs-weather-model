34aea7056a82734753c1dffd0bac7624466cf88d
Cameron Book <cbook@hercules-login-2.hpc.msstate.edu>
2025-05-20 13:39:27 -0500

    Remove fv3_conf sed commanfs from ufs_test.sh.

--- tests-dev/ufs_test.sh
+++ tests-dev/ufs_test.sh
@@ -217,15 +217,6 @@ if [[ ${LINK_TESTS} == true ]]; then
 
 fi
 
-if [[ ${SRT_NAME} == "tropical_cyclone" ]]; then
-  if [[ ${MACHINE_ID} == "hercules" ]]; then
-    sed -i 's/@\[TASKS\]/220/g' "${PATHRT}"/fv3_conf/fv3_slurm.IN_hercules
-  elif [[ ${MACHINE_ID} == "noaacloud" ]]; then
-    sed -i 's/@\[TASKS\]/220/g' "${PATHRT}"/fv3_conf/fv3_slurm.IN_noaacloud
-  fi
-fi
-
-
 #Check to error out if incompatible options are chosen together
 [[ ${KEEP_RUNDIR} == true && ${delete_rundir} == true ]] && die "-k and -d options cannot be used at the same time"
 [[ ${ECFLOW} == true && ${ROCOTO} == true ]] && die "-r and -e options cannot be used at the same time"
