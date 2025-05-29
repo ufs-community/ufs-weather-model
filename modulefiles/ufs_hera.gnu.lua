719c3d2e7e38915841c25fd55271f91ea6d5d6e0
RatkoVasic-NOAA <37597874+RatkoVasic-NOAA@users.noreply.github.com>
2025-05-29 11:46:49 -0400

    Fix stack-gcc version

--- modulefiles/ufs_hera.gnu.lua
+++ modulefiles/ufs_hera.gnu.lua
@@ -7,7 +7,7 @@ prepend_path("MODULEPATH", "/contrib/spack-stack/installs/scotch-gnu/modulefiles
 prepend_path("MODULEPATH", "/contrib/spack-stack/installs/gnu/modulefiles")
 prepend_path("MODULEPATH", "/contrib/spack-stack/installs/openmpi/modulefiles")
 
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "13.2.0"
+stack_gnu_ver=os.getenv("stack_gnu_ver") or "13.3.0"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
 stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "4.1.6"
