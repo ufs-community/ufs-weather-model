87c080636b31c77b4959a21ea3164eabf09b41f3
ulmononian <cameron.book@noaa.gov>
2025-06-05 19:41:03 +0000

    Fix LLVM lua file fortran compilers.

--- modulefiles/ufs_ursa.intelllvm.lua
+++ modulefiles/ufs_ursa.intelllvm.lua
@@ -3,6 +3,7 @@ loads UFS Model prerequisites for Ursa/Intel LLVM
 ]])
 
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.1/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/scratch4/NAGAPE/epic/role-epic/modulefiles")
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
@@ -24,6 +25,8 @@ setenv("FC", "mpiifx")
 setenv("I_MPI_CC", "icx")
 setenv("I_MPI_CXX", "icpx")
 setenv("I_MPI_FC", "ifx")
+setenv("I_MPI_F90", "ifx")
+setenv("I_MPI_F77", "ifx")
 
 setenv("CMAKE_Platform", "ursa.intel")
 
