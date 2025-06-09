588793cd1f82a5bdca055011fd8f84f9ff04f05a
denise.worthen <Denise.Worthen@noaa.gov>
2025-06-09 16:53:23 -0400

    commit temp testing changes

--- tests/rt.sh
+++ tests/rt.sh
@@ -1033,7 +1033,8 @@ if [[ "${CREATE_BASELINE}" == false ]] ; then
   fi
 fi
 
-INPUTDATA_ROOT=${INPUTDATA_ROOT:-${DISKNM}/NEMSfv3gfs/input-data-20250507}
+INPUTDATA_ROOT=/gpfs/f6/drsa-hurr1/proj-shared/Denise.Worthen/input-data-20250630
+#INPUTDATA_ROOT=${INPUTDATA_ROOT:-${DISKNM}/NEMSfv3gfs/input-data-20250507}
 INPUTDATA_ROOT_WW3=${INPUTDATA_ROOT}/WW3_input_data_20250225
 INPUTDATA_LM4=${INPUTDATA_LM4:-${INPUTDATA_ROOT}/LM4_input_data}
 
@@ -1135,7 +1136,7 @@ if [[ ${ECFLOW} == true ]]; then
   # Default maximum number of compile and run jobs
   MAX_BUILDS=10 #Max build jobs
   MAX_JOBS=30   #Max test/run jobs
-  ECF_TRIES=2   #Tries before failure
+  ECF_TRIES=1   #Tries before failure
 
   # Reduce maximum number of compile jobs on jet and s4 because of licensing issues
   if [[ ${MACHINE_ID} = jet ]]; then
