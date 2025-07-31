0c1524f9064a0523d585e737560661edbf3edc2e
Ufuk Turuncoglu <turuncu@ucar.edu>
2025-07-31 12:14:19 -0500

    update frontera module file - spack-stack 1.9.2

--- modulefiles/ufs_frontera.intel.lua
+++ modulefiles/ufs_frontera.intel.lua
@@ -5,39 +5,38 @@ loads UFS Model prerequisites for Frontera/Intel
 prepend_path("MODULEPATH", "/work2/06146/tg854455/frontera/spack-stack/modulefiles")
 load("ecflow/5.8.4")
 
-prepend_path("MODULEPATH", "/work2/01118/tg803972/frontera/spack-stack/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work2/01118/tg803972/frontera/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work2/01118/tg803972/frontera/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/intel/23.1.0")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "19.1.1.217"
+stack_intel_ver=os.getenv("stack_intel_ver") or "23.1.0"
 load(pathJoin("stack-intel", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2020.4.304"
-load(pathJoin("stack-intel-mpi", stack_impi_ver))
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
+load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.24.2"
 load(pathJoin("cmake", cmake_ver))
---load("cmake/3.24.2")
 
---load("ufs_common")
 --quick fix for https://github.com/JCSDA/spack-stack/issues/1388
 local ufs_modules = {
   {["jasper"]          = "2.0.32"},
-  {["zlib"]            = "1.2.13"},
+  --{["zlib"]            = "1.2.13"},
   {["libpng"]          = "1.6.37"},
-  {["hdf5"]            = "1.14.0"},
+  {["hdf5"]            = "1.14.3"},
   {["netcdf-c"]        = "4.9.2"},
   {["netcdf-fortran"]  = "4.6.1"},
-  {["parallelio"]      = "2.5.10"},
-  {["esmf"]            = "8.6.0"},
-  {["fms"]             = "2023.04"},
+  {["parallelio"]      = "2.6.2"},
+  {["esmf"]            = "8.8.0"},
+  {["fms"]             = "2024.02"},
   {["bacio"]           = "2.4.1"},
-  {["crtm"]            = "2.4.0"},
-  {["g2"]              = "3.4.5"},
-  {["g2tmpl"]          = "1.10.2"},
-  {["ip"]              = "4.3.0"},
+  {["crtm"]            = "3.1.1-build1"},
+  {["g2"]              = "3.5.1"},
+  {["g2tmpl"]          = "1.13.0"},
+  {["ip"]              = "5.1.0"},
   {["sp"]              = "2.5.0"},
   {["w3emc"]           = "2.10.0"},
-  {["gftl-shared"]     = "1.6.1"},
-  {["mapl"]            = "2.40.3-esmf-8.6.0"},
+  {["gftl-shared"]     = "1.9.0"},
+  {["mapl"]            = "2.53.4"},
   {["scotch"]          = "7.0.4"},
 }
 
@@ -48,7 +47,7 @@ for i = 1, #ufs_modules do
   end
 end
 
-stack_python_ver=os.getenv("stack_python_ver") or "3.10.13"
+stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
 load(pathJoin("stack-python", stack_python_ver))
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
