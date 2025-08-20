e99fc8b641e8017d47b6466ece0f8eff684cb90a
Wei Huang <Wei.Huang@noaa.gov>
2025-08-20 16:27:46 +0000

    add gnu to enable scotch

--- modulefiles/ufs_container.intel.lua
+++ modulefiles/ufs_container.intel.lua
@@ -3,6 +3,7 @@ Load environment to compile ufs-weather-model in a container using Intel
 ]])
 
 prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/opt/spack-stack/spack-stack-1.9.2/envs/unified-env/install/modulefiles/intel-oneapi-mpi/2021.13-argr3sd/gcc/11.4.0")
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.0"
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
@@ -15,7 +16,7 @@ load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
 
-nccmp_ver=os.getenv("nccmp_ver") or "1.9.1.0"
+nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
 
 setenv("CC", "mpiicx")
