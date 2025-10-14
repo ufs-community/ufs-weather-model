579678f7ff40f85730f6b007c579c76099492a1b
Denise Worthen <denise.worthen@noaa.gov>
2025-10-14 09:04:58 -0400

    point to input-data and remove rt.sfs

--- tests/rt.sh
+++ tests/rt.sh
@@ -1057,8 +1057,7 @@ if [[ "${CREATE_BASELINE}" == false ]] ; then
   fi
 fi
 
-INPUTDATA_ROOT=/gpfs/f6/infra-cpu/world-shared/Denise.Worthen/input-data-2025XXXX
-#INPUTDATA_ROOT=${INPUTDATA_ROOT:-${DISKNM}/NEMSfv3gfs/input-data-20250507}
+INPUTDATA_ROOT=${INPUTDATA_ROOT:-${DISKNM}/NEMSfv3gfs/input-data-20251015}
 INPUTDATA_ROOT_WW3=${INPUTDATA_ROOT}/WW3_input_data_20250807
 INPUTDATA_LM4=${INPUTDATA_LM4:-${INPUTDATA_ROOT}/LM4_input_data}
 
@@ -1160,7 +1159,7 @@ if [[ ${ECFLOW} == true ]]; then
   # Default maximum number of compile and run jobs
   MAX_BUILDS=10 #Max build jobs
   MAX_JOBS=30   #Max test/run jobs
-  ECF_TRIES=1   #Tries before failure
+  ECF_TRIES=2   #Tries before failure
 
   # Reduce maximum number of compile jobs on jet and s4 because of licensing issues
   if [[ ${MACHINE_ID} = jet ]]; then
