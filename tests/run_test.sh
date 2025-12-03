944362bd2c099d9bf953fbbe206efbe18db004ad
Denise Worthen <denise.worthen@noaa.gov>
2025-12-03 07:47:29 -0500

    parse the .IN as default

--- tests/run_test.sh
+++ tests/run_test.sh
@@ -316,13 +316,13 @@ if [[ ${DATM_CDEPS} = 'true' ]] || [[ ${S2S} = 'true' ]]; then
   if [[ ${HAFS} = 'false' ]]; then
     atparse < "${PATHRT}/parm/ice_in.IN" > ice_in
     atparse < "${PATHRT}/parm/${MOM6_INPUT:-MOM_input_${OCNRES}.IN}" > INPUT/MOM_input
-    atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template}" > diag_table
+    atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template.IN}" > diag_table
     atparse < "${PATHRT}/parm/MOM6_data_table.IN" > data_table
   fi
 fi
 
 if [[ ${HAFS} = 'true' ]] && [[ ${DATM_CDEPS} = 'false' ]]; then
-  atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template}" > diag_table
+  atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template.IN}" > diag_table
 fi
 
 if [[ "${DIAG_TABLE_ADDITIONAL:-}Q" != Q ]]; then
@@ -338,7 +338,7 @@ fi
 
 # ATMAERO
 if [[ ${CPLCHM} == .true. ]] && [[ ${S2S} = 'false' ]]; then
-  atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template}" > diag_table
+  atparse < "${PATHRT}/parm/diag_table/${DIAG_TABLE:-diag_table_template.IN}" > diag_table
 fi
 
 if [[ ${DATM_CDEPS} = 'true' ]]; then
