c4cfc72183bf0b98c6dde1546bc89d4a0029860a
Ratko Vasic <ratko.vasic@noaa.gov>
2026-01-29 15:42:02 +0000

    Switch from control_c48.v2.sfc to control_c48

--- tests-dev/ufs_test.sh
+++ tests-dev/ufs_test.sh
@@ -241,16 +241,18 @@ shift $((OPTIND-1))
 [[ $# -gt 1 ]] && usage
 
 #############################################################################
-# For aquaplanet case we need to change the radiation code:
+# For aquaplanet case we need to change the radiation code,
+# as well as supress warnings for land points:
 #############################################################################
 if [[ ${SRT_NAME} == aquaplanet ]]; then
-  #--- find radiation_astronomy.f file
+
+#--- find radiation_astronomy.f file
   RAD_FILE=`find ../ -name radiation_astronomy.f`
-  #--- if we cannot find radiation_astronomy.f file, then exit:
+#--- if we cannot find radiation_astronomy.f file, then exit:
   [[ -z $RAD_FILE ]] && die "cannot find radiation_astronomy.f file"
-  #--- find if file is original (grep text containing word AQUAPLANET)
+#--- find if file is original (grep text containing word AQUAPLANET)
   RAD_FILE_EDITED=`grep "AQUAPLANET TEST CASE" $RAD_FILE | wc -l`
-  #--- if file is original, then add lines to appropriate place
+#--- if file is original, then add lines to appropriate place
   if [[ ${RAD_FILE_EDITED} == 0 ]]; then
 sed -i '/solcon = solc0/a\
 \
@@ -259,6 +261,22 @@ sed -i '/solcon = solc0/a\
       dlt=0.\
 ! AQUAPLANET TEST CASE' $RAD_FILE
   fi
+
+#--- find fv_diagnostics.F90 file
+  DIAG_FILE=`find ../ -name fv_diagnostics.F90`
+#--- if we cannot find find ../ -name fv_diagnostics.F90 file, then exit:
+  [[ -z $DIAG_FILE ]] && die "cannot find fv_diagnostics.F90 file"
+#--- find if file is original (grep text containing word AQUAPLANET)
+  DIAG_FILE_EDITED=`grep "AQUAPLANET TEST CASE" $DIAG_FILE | wc -l`
+#--- if file is original, then add lines to appropriate place
+  if [[ ${DIAG_FILE_EDITED} == 0 ]]; then
+sed -i '/Print out where/a\
+\
+! AQUAPLANET TEST CASE\
+      return\
+! AQUAPLANET TEST CASE' $DIAG_FILE
+  fi
+
 fi
 #############################################################################
 
