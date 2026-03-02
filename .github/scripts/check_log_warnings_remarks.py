44e9c2f2e8dd0cc3c0b500e1eb69a1e7938cea26
Gillian Petro <gillian.petro@noaa.gov>
2026-03-02 09:02:46 -0500

    chg - to *

--- .github/scripts/check_log_warnings_remarks.py
+++ .github/scripts/check_log_warnings_remarks.py
@@ -120,7 +120,7 @@ def print_html_results(dict):
          if results[category]:
             mdFile.write(f"\n<h3>{machine.upper()}</h3>")
             unordered_list = [f"{category.title()}:", dict[machine][category]]
-            mdFile.new_list(unordered_list, marked_with='-')
+            mdFile.new_list(unordered_list, marked_with='*')
    return mdFile.get_md_text()
 
 def main():
