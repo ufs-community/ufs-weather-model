387d27bb3a9b88d99725686b6da033a579f5c818
todd arbetter <todd.e.arbetter@gmail.com>
2026-03-09 15:04:22 -0600

    Update dprefix to use epic directory

--- tests/rt.sh
+++ tests/rt.sh
@@ -842,7 +842,7 @@ case ${MACHINE_ID} in
     COMPILE_QUEUE="batch"
     PARTITION="orion"
   # dprefix=${dprefix:-"/work/noaa/stmp/${USER}"}
-    dprefix=${dprefix:-"/work2/noaa/${USER}"}
+    dprefix=${dprefix:-"/work2/noaa/epic/${USER}"}
     DISKNM="/work2/noaa/epic/UFS-WM_RT"
     STMP="${dprefix}/stmp"
     PTMP="${dprefix}/stmp"
@@ -871,7 +871,7 @@ case ${MACHINE_ID} in
     COMPILE_QUEUE="batch"
     PARTITION="hercules"
   # dprefix=${dprefix:-"/work2/noaa/stmp/${USER}"}
-    dprefix=${dprefix:-"/work2/noaa/${USER}"}
+    dprefix=${dprefix:-"/work2/noaa/epic/${USER}"}
     DISKNM="/work2/noaa/epic/hercules/UFS-WM_RT"
     STMP="${dprefix}/stmp"
     PTMP="${dprefix}/stmp"
