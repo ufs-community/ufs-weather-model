b03edc000d2897ad10c8e08274ef26821563d22f
Natalie Perlin <Natalie.Perlin@noaa.gov>
2026-03-11 18:59:26 -0600

    Updated spack-stack-1.9.2 with corrected zlib and glibc versions

--- modulefiles/ufs_derecho.intel.lua
+++ modulefiles/ufs_derecho.intel.lua
@@ -3,8 +3,8 @@ loads UFS Model prerequisites for NOAA Parallelworks/Intel
 ]])
 
 setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/cray-mpich/8.1.29-3sepg3g/gcc/12.4.0")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-zlib.1.2.13/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-zlib.1.2.13/install/modulefiles/cray-mpich/8.1.29-3sepg3g/gcc/12.4.0")
 
 unload("ncarcompilers")
 stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
