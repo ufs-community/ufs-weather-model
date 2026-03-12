0f485ec3fea2bc94aa886748466527834ce56dbd
Natalie Perlin <Natalie.Perlin@noaa.gov>
2026-03-11 19:22:05 -0600

    Updated ufs_derecho.gnu.lua to use spack-stack-1.9.2 with corrected zlib and glibc versions

--- modulefiles/ufs_derecho.gnu.lua
+++ modulefiles/ufs_derecho.gnu.lua
@@ -3,7 +3,8 @@ loads UFS Model prerequisites for Derecho/GNU
 ]])
 
 setenv("LMOD_TMOD_FIND_FIRST","yes")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-gcc-12.4/install/modulefiles/Core")
+--prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-gcc-12.4/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-gcc-12.4-zlib-1.2.13/install/modulefiles/Core")
 
 unload("ncarcompilers")
 stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.4.0"
