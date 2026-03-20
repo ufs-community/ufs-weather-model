0f75719d13af3f8896d0f08a48014f1906cf07e4
Daniel Sarmiento <daniel.sarmiento@noaa.gov>
2026-03-20 15:01:29 +0000

    MOM6 filename changes

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -1647,7 +1647,7 @@ export_mom6() {
   export DT_THERM_MOM6=3600
   export MOM6_INPUT=MOM_input_100.IN
   export MOM6_OUTPUT_DIR=./MOM6_OUTPUT
-  export MOM6_OUTPUT_FH=6
+  export MOM6_HISTFREQ_N=6
   export MOM6_RESTART_DIR=./RESTART/
   export MOM6_RESTART_SETTING=n
   export MOM6_RIVER_RUNOFF=False
