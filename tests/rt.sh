b2ffa6ec66918813be6398f2e78cb53215fcfa55
Natalie Perlin <Natalie.Perlin@noaa.gov>
2026-03-23 15:04:13 -0600

    Allow no rocoto or ecflow choice on Derecho

--- tests/rt.sh
+++ tests/rt.sh
@@ -951,17 +951,14 @@ case ${MACHINE_ID} in
     if [[ "${ROCOTO:-false}" == true ]] ; then
       module use /glade/work/epicufsrt/contrib/derecho/modulefiles
       module load rocoto/1.3.7
-    elif [[ "${ECFLOW:-false}" == true ]] ; then
+    fi
+    if [[ "${ECFLOW:-false}" == true ]] ; then
       module use /glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/oneapi/2024.2.1
       module load stack-python/3.11.7
       module load ecflow/5.11.4
       ECF_HOST=$(hostname)
       ECF_PORT=$(( $(id -u) + 1500 ))
       export ECF_PORT ECF_HOST
-    else
-      echo "Current workflow scheduler setting: ECFLOW=$ECFLOW, ROCOTO=$ROCOTO"
-      echo "Either the EcFlow (-e) or Rocoto (-r) flag must be chosen when running rt.sh on Derecho. Exiting..."
-      exit 1
     fi
 
     QUEUE="main"
