be20273597a9e3be2c8b33867ef9c07941f2c9e1
Daniel Sarmiento <daniel.sarmiento@noaa.gov>
2026-04-15 15:30:19 +0000

    Add logs and changes

--- tests/rt.sh
+++ tests/rt.sh
@@ -1008,7 +1008,7 @@ if [[ ${skip_check_results} == true ]]; then
 else
   REGRESSIONTEST_LOG=${PATHRT}/logs/RegressionTests_${MACHINE_ID}.log
 fi
-rm -f "${REGRESSIONTEST_LOG}"
+mv "${REGRESSIONTEST_LOG}" "${REGRESSIONTEST_LOG}.bak"
 
 TEST_START_TIME="$(date '+%Y%m%d %T')"
 export TEST_START_TIME
