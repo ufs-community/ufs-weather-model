9ccdf0fd230f4815d7559596fbf01049f32efe73
todd arbetter <todd.e.arbetter@gmail.com>
2026-04-17 09:57:20 -0600

    Fix echo command in disk_usage function

--- .cicd/scripts/disk_usage.sh
+++ .cicd/scripts/disk_usage.sh
@@ -37,7 +37,7 @@ function disk_usage() {
     local directory="${1:-${PWD}}"
     local depth="${2:-1}"
     local size="${3:-k}"
-    echo "Disk usage: ${JOB_NAME:-ci}/${UFS_PLATFORM}/$(basename \"${directory}\")"
+    echo "Disk usage: ${JOB_NAME:-ci}/${UFS_PLATFORM}/$(basename ${directory})"
     (
     cd "${directory}" || exit 1
     echo "Platform,Build,Owner,Group,Inodes,${size:-k}bytes,Access Time,Filename"
