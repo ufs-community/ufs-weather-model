45dff8b1f67526f818be0374a60a296f91bdaad7
Gillian Petro <gillian.petro@noaa.gov>
2026-09-14 23:23:54 -0400

    revert derecho to ss1.9.2

--- modulefiles/ufs_derecho.gnu.lua
+++ modulefiles/ufs_derecho.gnu.lua
@@ -3,34 +3,29 @@ loads UFS Model prerequisites for Derecho/GNU
 ]])
 
 setenv("LMOD_TMOD_FIND_FIRST","yes")
+prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-1.9.2/envs/ue-gcc-12.4-zlib-1.2.13/install/modulefiles/Core")
 
-prepend_path("MODULEPATH", "/opt/cray/pe/modulefiles")
-load("crayenv/25.03")
-
---prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-2.1.1/envs/ue-gcc-13.3.1/modules/Core")
-prepend_path("MODULEPATH", "/glade/work/epicufsrt/contrib/spack-stack/derecho/spack-stack-2.1.1/envs/ue-gcc-13.3.1-build/modules/Core")
-
-
-stack_gnu_ver=os.getenv("stack_gnu_ver") or "13.3.1"
+unload("ncarcompilers")
+stack_gnu_ver=os.getenv("stack_gnu_ver") or "12.4.0"
 load(pathJoin("stack-gcc", stack_gnu_ver))
 
---stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
-stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.32"
+stack_cray_mpich_ver=os.getenv("stack_cray_mpich_ver") or "8.1.29"
 load(pathJoin("stack-cray-mpich", stack_cray_mpich_ver))
 
-unload("cray-libsci")
-
-cmake_ver=os.getenv("cmake_ver") or "3.31.8"
+cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
+
+stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
+load(pathJoin("stack-python", stack_python_ver))
 
 load("ufs_common")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
 
-setenv("CC", "mpicc")
-setenv("CXX", "mpicxx")
-setenv("FC", "mpif90")
+setenv("CC","cc")
+setenv("CXX","CC")
+setenv("FC","ftn")
 setenv("CMAKE_Platform", "derecho.gnu")
 
 whatis("Description: UFS build environment")
