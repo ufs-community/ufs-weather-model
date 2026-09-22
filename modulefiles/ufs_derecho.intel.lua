09438c1376b3e07694b729bab66df6548c3e6518
gspetro-NOAA <gillian.petro@noaa.gov>
2026-09-22 11:53:18 -0600

    add back derecho intel modulefile

--- modulefiles/ufs_derecho.intel.lua
+++ modulefiles/ufs_derecho.intel.lua
@@ -19,7 +19,32 @@ load(pathJoin("cmake", cmake_ver))
 stack_python_ver=os.getenv("stack_python_ver") or "3.11.7"
 load(pathJoin("stack-python", stack_python_ver))
 
-load("ufs_common")
+local ufs_modules = {
+  {["jasper"]          = "2.0.32"},
+  {["libpng"]          = "1.6.37"},
+  {["hdf5"]            = "1.14.3"},
+  {["netcdf-c"]        = "4.9.2"},
+  {["netcdf-fortran"]  = "4.6.1"},
+  {["parallelio"]      = "2.6.2"},
+  {["esmf"]            = "8.8.0"},
+  {["fms"]             = "2024.02"},
+  {["bacio"]           = "2.4.1"},
+  {["crtm"]            = "2.4.0.1"},
+  {["g2"]              = "3.5.1"},
+  {["g2tmpl"]          = "1.13.0"},
+  {["ip"]              = "5.1.0"},
+  {["w3emc"]           = "2.10.0"},
+  {["gftl-shared"]     = "1.9.0"},
+  {["mapl"]            = "2.53.4-esmf-8.8.0"},
+  {["scotch"]          = "7.0.4"},
+}
+
+for i = 1, #ufs_modules do
+  for name, default_version in pairs(ufs_modules[i]) do
+    local env_version_name = string.gsub(name, "-", "_") .. "_ver"
+    load(pathJoin(name, os.getenv(env_version_name) or default_version))
+  end
+end
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
